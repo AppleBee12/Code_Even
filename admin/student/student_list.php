18b7bb1086baa002ec0cb041bf8cf4e0de960f39
YUNA9627 <yuna2175@naver.com>
2024-11-13 01:15:35 +0900

    📝 docs: 파일 이동

--- admin/student/student_list.php
+++ admin/student/student_list.php
@@ -2,7 +2,9 @@
 include_once($_SERVER['DOCUMENT_ROOT']. '/code_even/admin/inc/header.php');
 ?>
 
-<div class=""></div>
+<div class="">
+  <h1>어쩌구</h1>
+</div>
 
 <?php
 include_once($_SERVER['DOCUMENT_ROOT']. '/code_even/admin/inc/footer.php');
