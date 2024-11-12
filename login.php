5de577b84261e6bb61743b87b861110123419299
chaerimjo-web <chaerim010710@gmail.com>
2024-11-12 18:21:02 +0900

    📝 docs: 이미지추가

--- login.php
+++ login.php
@@ -4,6 +4,7 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
 <div class="container_login ">
+  <img src="images/txt_logo.png" alt="">
   <h1>로그인</h1>
   <h2>관리자 로그인</h2>
   <form action="login_ok.php" method="POST">
