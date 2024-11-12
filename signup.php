10891ee45e676b0bbc674bbce8e3a9b0df4c5eda
chaerimjo-web <chaerim010710@gmail.com>
2024-11-12 21:04:50 +0900

    📝 docs: add signup.php

---
+++ signup.php
@@ -0,0 +1,16 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>회원가입</h1>
+
+  <label for="inputPassword5" class="form-label">이름</label>
+<input type="password" id="inputPassword5" class="form-control w-25" aria-describedby="passwordHelpBlock" placeholder="홍길동">
+</body>
+</html>
