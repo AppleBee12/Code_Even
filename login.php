9bc4b1484b4e002ca413ff1345e4224d77fcf4c4
chaerimjo-web <chaerim010710@gmail.com>
2024-11-13 00:31:43 +0900

    📝 docs: name값 추가

--- login.php
+++ login.php
@@ -1,14 +1,14 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-<div class="container_login ">
+<div class="container">
   <img src="images/txt_logo.png" alt="">
-  <h1>로그인</h1>
+  <h1 class="mt-3">로그인</h1>
   <h2>관리자 로그인</h2>
   <form action="login_ok.php" method="POST">
       <label for="inputPassword5" class="form-label  mt-3">아이디</label>
-      <input type="text" id="inputPassword5" class="form-control  w-25" aria-describedby="passwordHelpBlock" placeholder="아이디를 입력하세요" required>
+      <input type="text" id="inputPassword5" class="form-control  w-25" aria-describedby="passwordHelpBlock" placeholder="아이디를 입력하세요" name="userid" required>
       <label for="inputPassword5" class="form-label mt-3">비밀번호</label>
-      <input type="password" id="inputPassword5" class="form-control  w-25" aria-describedby="passwordHelpBlock" placeholder="비밀번호를 입력하세요" required>
+      <input type="password" id="inputPassword5" class="form-control  w-25" aria-describedby="passwordHelpBlock" placeholder="비밀번호를 입력하세요" name="userpw" required>
 
     <div class="d-flex flex-column justify-content-center">
       <button class="btn btn-primary mt-3 w-25">로그인</button>
@@ -19,4 +19,5 @@
       </div>
     </div>
   </form>
+  
 </div>
