f371525b5037f0b2ecec716877d955a768212709
AppleBee12 <mieldeluna@naver.com>
2024-11-14 02:24:32 +0900

    🚚 rename: 프로필항목 index->header이동

--- index.php
+++ index.php
@@ -14,38 +14,7 @@ if(!isset($_SESSION['AUID'])){
 
 
 <div class="container ">
-  <?php
-    if(!isset($_SESSION['AUID'])){
-  ?>
-    <a href="admin/login/login.php">로그인</a>
-  <?php
-    }else{
-  ?>
-    <div><?= $_SESSION['AUNAME'] ?> 관리자님 <br>환영합니다. </div>
-    <!-- <a href="logout.php">로그아웃</a> -->
-
-
-    <ul class="nav nav-pills">
-    <li class="nav-item">
-    <a class="nav-link" href="#"><img src="images/sb_logo.png" alt="" width="50"></a>
-  </li>
-  <li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false"></a>
-    <ul class="dropdown-menu">
-      <li><a class="dropdown-item" href="admin/login/logout.php">로그아웃</a></li>
-      <li><a class="dropdown-item" href="#">Another action</a></li>
-      <li><a class="dropdown-item" href="#">Something else here</a></li>
-      <li><hr class="dropdown-divider"></li>
-      <li><a class="dropdown-item" href="#">Separated link</a></li>
-    </ul>
-  </li>
-  
-</ul>
-
-
-  <?php
-    }
-  ?>
+ 
   
 </div>
 
