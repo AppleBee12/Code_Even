8a1e5d5960c3f38818924787b3c961c9fe3737d6
YUNA9627 <yuna2175@naver.com>
2024-11-15 04:46:29 +0900

    📝 docs: 수강생 목록 페이지 form 태그 추가 및 구조 변경

--- admin/student/student_list.php
+++ admin/student/student_list.php
@@ -16,90 +16,129 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/code_even/admin/inc/header.php');
     </div>
   </form>
 
-  <table class="table list_table">
-    <thead>
-      <tr>
-        <th scope="col">
-          <input class="form-check-input" type="checkbox" value="" id="allCheck">
-        </th>
-        <th scope="col">번호</th>
-        <th scope="col">아이디</th>
-        <th scope="col">이름</th>
-        <th scope="col">강좌명</th>
-        <th scope="col">진도율</th>
-        <th scope="col">수강이수</th>
-        <th scope="col">학습기간</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th scope="row">
-          <input class="form-check-input itemCheckbox" type="checkbox" value="">
-        </th>
-        <td>2</td>
-        <td><a href="student_details.php" class="underline">ping09</a></td>
-        <td><a href="student_details.php" class="underline">피곤핑</a></td>
-        <td>기초부터 확실하게! 페이지의..</td>
-        <td>0%</td>
-        <td>
-          <span class="badge text-bg-light">미이수</span>
-        </td>
-        <td>2024.10.19 ~ 2024.10.31</td>
-      </tr>
-      <tr>
-        <th scope="row">
-          <input class="form-check-input itemCheckbox" type="checkbox" value="" id="flexCheckDefault">
-        </th>
-        <td>1</td>
-        <td><a href="student_details.php" class="underline">hong1234</a></td>
-        <td><a href="student_details.php" class="underline">홍길동</a></td>
-        <td>기초부터 확실하게! 페이지의..</td>
-        <td>100%</td>
-        <td>
-          <button id="printButton">
-            <span class="badge text-bg-dark">이수증</span>
-          </button>
-        </td>
-        <td>2024.10.19 ~ 2024.10.31</td>
-      </tr>
-    </tbody>
-  </table>
+  <form action="">
+    <table class="table list_table">
+      <thead>
+        <tr>
+          <th scope="col">
+            <input class="form-check-input" type="checkbox" value="" id="allCheck">
+          </th>
+          <th scope="col">번호</th>
+          <th scope="col">아이디</th>
+          <th scope="col">이름</th>
+          <th scope="col">강좌명</th>
+          <th scope="col">진도율</th>
+          <th scope="col">수강이수</th>
+          <th scope="col">학습기간</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">
+            <input class="form-check-input itemCheckbox" type="checkbox" value="">
+          </th>
+          <td>2</td>
+          <td><a href="student_details.php" class="underline">ping09</a></td>
+          <td><a href="student_details.php" class="underline">피곤핑</a></td>
+          <td>기초부터 확실하게! 페이지의..</td>
+          <td>0%</td>
+          <td>
+            <span class="badge text-bg-light">미이수</span>
+          </td>
+          <td>2024.10.19 ~ 2024.10.31</td>
+        </tr>
+        <tr>
+          <th scope="row">
+            <input class="form-check-input itemCheckbox" type="checkbox" value="" id="flexCheckDefault">
+          </th>
+          <td>1</td>
+          <td><a href="student_details.php" class="underline">hong1234</a></td>
+          <td><a href="student_details.php" class="underline">홍길동</a></td>
+          <td>기초부터 확실하게! 페이지의..</td>
+          <td>100%</td>
+          <td>
+            <button type="button" id="printButton">
+              <span class="badge text-bg-dark">이수증</span>
+            </button>
+          </td>
+          <td>2024.10.19 ~ 2024.10.31</td>
+        </tr>
+      </tbody>
+    </table>
 
-  <div class="modal" id="send_email" tabindex="-1">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title">이메일 전송</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          <div class="table modal_table">
-            <div>
-              <div class="d-flex">
-                <div>제목 <b>*</b></div>
-                <input type="text" class="form-control form-control-sm" placeholder="제목을 입력해주세요.">
-              </div>
-            </div>
-            <div>
-              <div class="d-flex">
-                <div>내용 <b>*</b></div>
-                <textarea name="" class="form-control" placeholder="메시지를 입력해주세요."></textarea>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-outline-danger" data-bs-dismiss="modal">취소</button>
-          <button type="button" class="btn btn-secondary">등록</button>
-        </div>
-      </div>
+    <div class="d-flex justify-content-end">
+       <button type="button" data-bs-toggle="modal" data-bs-target="#send_email" class="btn btn-outline-secondary">이메일
+         전송</button>
+     </div>
+
+     <!-- //Pagination -->
+    <div class="list_pagination" aria-label="Page navigation example">
+      <ul class="pagination d-flex justify-content-center">
+        <li class="page-item">
+          <a class="page-link" href="" aria-label="Previous">
+            <i class="bi bi-chevron-left"></i>
+          </a>
+        </li>
+        <li class="page-item active"><a class="page-link" href="">1</a></li>
+        <li class="page-item"><a class="page-link" href="">2</a></li>
+        <li class="page-item"><a class="page-link" href="">3</a></li>
+        <li class="page-item">
+          <a class="page-link" href="" aria-label="Next">
+            <i class="bi bi-chevron-right"></i>
+          </a>
+        </li>
+      </ul>
     </div>
-  </div>
+   
+    <!-- //email 모달창 -->
+     <div class="modal modal-lg" id="send_email" tabindex="-1">
+       <div class="modal-dialog">
+         <div class="modal-content">
+           <div class="modal-header">
+             <h5 class="modal-title">이메일 전송</h5>
+             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+           </div>
+           <div class="modal-body">
+           <form action="" method="">
+            <table class="table">
+              <colgroup>
+                <col style="width:110px">
+                <col style="width:250px">
+              </colgroup>
+              <thead class="thead-hidden">
+                <tr>
+                  <th scope="col">구분</th>
+                  <th scope="col">내용</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr class="none">
+                  <th scope="row">이름(아이디)</th>
+                  <td>홍길동</td>
+                  <th scope="row">이메일</th>
+                  <td>hong1234@hong.com</td>
+                </tr>
+                <tr class="none">
+                  <th scope="row">제목 <b>*</b></th>
+                  <td colspan="3"><input type="text" class="form-control"></td>
+                </tr>
+                <tr class="none">
+                  <th scope="row">내용 <b>*</b></th>
+                  <td colspan="3"><textarea name="" id="" class="form-control"></textarea></td>
+                </tr>
+              </tbody>
+            </table>
+          </form>
+           </div>
+           <div class="modal-footer">
+             <button type="button" class="btn btn-outline-danger" data-bs-dismiss="modal">취소</button>
+             <button type="button" class="btn btn-secondary">등록</button>
+           </div>
+         </div>
+       </div>
+     </div>
 
-  <div class="d-flex justify-content-end">
-    <button type="button" data-bs-toggle="modal" data-bs-target="#send_email" class="btn btn-outline-secondary">이메일
-      전송</button>
-  </div>
+  </form>
 
 </div>
 
@@ -127,7 +166,6 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/code_even/admin/inc/header.php');
 
   document.getElementById("printButton").addEventListener("click", printPage);
 
-
   // "전체 선택" 체크박스를 가져옴
   const checkAll = document.getElementById('allCheck');
   // 각 항목 체크박스를 모두 가져옴
@@ -140,7 +178,6 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/code_even/admin/inc/header.php');
     });
   });
 </script>
-
 
 <?php
 include_once($_SERVER['DOCUMENT_ROOT'] . '/code_even/admin/inc/footer.php');
