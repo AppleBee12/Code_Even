e5e671b2f59285d8ea3364fc2c35c349e70ea503
AppleBee12 <mieldeluna@naver.com>
2024-11-14 18:24:30 +0900

    📝 docs: chart수정

--- admin/index.php
+++ admin/index.php
@@ -80,37 +80,42 @@ if(!isset($_SESSION['AUID'])){
   const linechart = document.getElementById('current_six_news');
   const cateDatas_new = [2000,2800,3200,3800,4000,4765,5500];
   const cateDatas_visit = [8000,6700,6900,8500,9215,7265];
-  const data = {
-  labels: generateLabels(),
-  datasets: [
-    {
-      label: 'Dataset',
-      data: generateData(),
-      borderColor: Utils.CHART_COLORS.red,
-      backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red),
-      fill: false
-    }
-  ]
-};
 
-const config = {
-  type: 'line',
-  data: data,
-  options: {
-    plugins: {
-      filler: {
-        propagate: false,
-      },
-      title: {
-        display: true,
-        text: (ctx) => 'Fill: ' + ctx.chart.data.datasets[0].fill
-      }
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['6월', '7월', '8월', '9월', '10월', '11월'],
+        datasets: [{
+            label: '명',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
     },
-    interaction: {
-      intersect: false,
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
     }
-  },
-};
+});
 
 </script>
 <?php
