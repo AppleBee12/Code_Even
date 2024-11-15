59030b268ae94643cf1e6cab3f33181f276cc6ee
YUNA9627 <yuna2175@naver.com>
2024-11-15 17:50:30 +0900

    📝 docs: sql 작성중

--- admin/inquiry/notice_write_ok.php
+++ admin/inquiry/notice_write_ok.php
@@ -3,4 +3,6 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/CODE_EVEN/admin/inc/dbcon.php');
 session_start();
 
 print_r($_POST);
+
+$sql = "INSERT INTO notice (ntid, title, content, view, regdate, status, file) VALUES ()";
 ?>
