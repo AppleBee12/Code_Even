0bf5424ddc18da2626dd5aeb6017cea8660655c3
YUNA9627 <yuna2175@naver.com>
2024-11-16 16:30:48 +0900

    📝 docs: 공지사항 수정 페이지 작성

---
+++ admin/inquiry/notice_modify_ok.php
@@ -0,0 +1,43 @@
+<?php
+include_once($_SERVER['DOCUMENT_ROOT'] . '/CODE_EVEN/admin/inc/dbcon.php');
+
+// print_r($_POST);
+$ntid = $_POST['ntid'];
+$username = $_POST['username'];
+$userid = $_POST['userid'];
+$title = $_POST['title'];
+$content = $_POST['content'];
+$status = $_POST['status'];
+
+$notice_sql = "
+    UPDATE notice 
+    SET 
+        title = '$title',
+        content = '$content',
+        status = '$status'
+    WHERE uid = (
+        SELECT uid 
+        FROM user 
+        WHERE username = '$username' AND userid = '$userid'
+    )
+    AND ntid = '$ntid'
+";
+
+$user_result = $mysqli->query($notice_sql);
+
+if ($user_result === true) {
+  echo
+    "<script>
+    confirm('글을 수정하시겠습니까?');
+    alert('수정이 완료되었습니다.');
+    location.href='notice.php';
+  </script>";
+} else {
+  echo
+    "<script>
+    alert('글쓰기 실패');
+    history.back();
+  </script>";
+}
+
+?>
