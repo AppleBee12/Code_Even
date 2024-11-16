4a552eede13f9561293edaa02839e379536ae754
AppleBee12 <mieldeluna@naver.com>
2024-11-16 20:26:29 +0900

    ♻️ refactor: main.js common.js분리/ 📝 docs: 차트 데이터 연결중

--- admin/inc/footer.php
+++ admin/inc/footer.php
@@ -1,6 +1,7 @@
   </div>
 </div>
-<script src="http://<?= $_SERVER['HTTP_HOST']; ?>/code_even/admin/js/main.js"></script>
+
+<script src="http://<?= $_SERVER['HTTP_HOST']; ?>/code_even/admin/js/footer.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="http://<?= $_SERVER['HTTP_HOST']; ?>/code_even/admin/js/summernote-bs5.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.20/dist/summernote-lite.min.js"
