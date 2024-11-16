1f34529085b601f059754465fe9a928edfc50c77
eunjin6-6 <dmswls616@gmail.com>
2024-11-17 02:45:46 +0900

    📝 docs: oder 상세페이지 내용 구성

--- admin/orders/orders_details.php
+++ admin/orders/orders_details.php
@@ -4,12 +4,12 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/code_even/admin/inc/header.php');
 ?>
 
 <div class="container">
-  <h2>강사 프로필 수정</h2>
+  <h2>주문결제상세</h2>
   <div class="content_bar">
-    <h3>강사 상세정보</h3>
+    <h3>주문상세목록</h3>
   </div>
   <table class="table list_table">
-      <thead>
+    <thead>
         <tr>
           <th scope="col">번호</th>
           <th scope="col">주문상세번호</th>
@@ -22,8 +22,8 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/code_even/admin/inc/header.php');
           <th scope="col">결제금액</th>
           <th scope="col">상태</th>
         </tr>
-      </thead>
-      <tbody>
+    </thead>
+    <tbody>
         <tr>
           <th scope="row">1</th>
           <td><a href="teacher_details.php">123</a></td>
@@ -36,7 +36,24 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/code_even/admin/inc/header.php');
           <td><a href="teacher_details.php">89,000</td>
           <td><a href="teacher_details.php">결제완료</td>
         </tr>   
-    </table>
+        <tr>
+          <th scope="row">1</th>
+          <td><a href="teacher_details.php">124</a></td>
+          <td><a href="teacher_details.php">교재</td>
+          <td><a href="orders_details.php">기초부터 확실하게! 페이지의 내용 전달을 위한...</td>
+          <td><a href="teacher_details.php">1</td>
+          <td><a href="teacher_details.php">13,000</td>
+          <td><a href="teacher_details.php">-</td>
+          <td><a href="teacher_details.php">0</td>
+          <td><a href="teacher_details.php">13,000</td>
+          <td><a href="teacher_details.php">결제완료</td>
+        </tr>
+    </tbody> 
+  </table>
+
+  <div class="content_bar">
+    <h3>결제 정보</h3>
+  </div>
   <form action="">
   <table class="table w-100 info_table">
     <colgroup>
@@ -49,98 +66,56 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/code_even/admin/inc/header.php');
       <tr>
         <th scope="row">이름 <b>*</b></th>
         <td>
-          <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="냠냠">
+          125
         </td>
-        <th scope="row">링크</th>
+        <th scope="row">아이디</th>
         <td>
-          <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="냠냠">
+          nany
         </td>
       </tr>
       <tr>
-        <th scope="row">아이디 <b>*</b></th>
+        <th scope="row">결제일 <b>*</b></th>
         <td>
-          <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="냠냠">
+          2024/10/29
         </td>
-        <th scope="row">은행명</th>
+        <th scope="row">이름</th>
         <td>
-          <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="냠냠">
-        </td>
-      </tr>
-      <tr>
-        <th scope="row">연락처 <b>*</b></th>
-        <td>
-          <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="냠냠">
-        </td>
-        <th scope="row">계좌번호</th>
-        <td>
-          <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="냠냠">
+          홍나니
         </td>
       </tr>
       <tr>
-        <th scope="row">이메일 <b>*</b></th>
+        <th scope="row">주문명</th>
         <td colspan="3">
-          <input type="text" class="form-control w_512" id="exampleFormControlInput1" placeholder="냠냠">
+        기초부터 확실하게! (페이지의 내용 전달을 위한 HTML, 스타일 설정을 위한 CSS 기초 학습) 외 1건
         </td>
       </tr>
       <tr>
-        <th scope="row">대표분야 <b>*</b></th>
+        <th scope="row">결제금액</th>
         <td colspan="3">
-          <select class="form-select w_512" aria-label="Default select example">
-
-            <option class="optionListItem" value="1" selected>웹개발</option>
-            <option value="2">클라우드</option>
-            <option value="3">보안</option>
-          </select>
+          102,000 원
         </td>
       </tr>
       <tr>
-        <td colspan="4">
-          <hr>
-        </td>
-      </tr>
-      <tr>
-        <th scope="row">승인상태</th>
-        <td>
+        <th scope="row">결제방법</th>
+        <td colspan="3">
           <div class="form-check form-check-inline">
             <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" checked>
-            <label class="form-check-label" for="inlineRadio1">심사중</label>
+            <label class="form-check-label" for="inlineRadio1">신용카드</label>
           </div>
           <div class="form-check form-check-inline">
-            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
-            <label class="form-check-label" for="inlineRadio2">승인완료</label>
+            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" disabled>
+            <label class="form-check-label" for="inlineRadio2">무통장입금</label>
           </div>
           <div class="form-check form-check-inline">
-            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3">
-            <label class="form-check-label" for="inlineRadio3">승인거절</label>
+            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3" disabled>
+            <label class="form-check-label" for="inlineRadio3">실시간 계좌이체</label>
           </div>
-        </td>
-        <th scope="row">강사전시옵션</th>
-        <td>
-        <div class="form-check form-check-inline d-inline-block">
-              <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-              <label class="form-check-label" for="flexCheckDefault">
-                신규
-              </label>
-            </div>
-            <div class="form-check d-inline-block">
-              <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-              <label class="form-check-label" for="flexCheckDefault">
-                추천
-              </label>
-            </div>
         </td>
       </tr>
       <tr>
         <td colspan="4">
             <hr>
         </td>
-      </tr>
-      <tr>
-        <th scope="row">소개글 <b>*</b></th>
-        <td colspan="3">
-        <div id="summernote"></div>
-        </td>
-
       </tr>
       
     </tbody>
