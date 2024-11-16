1ea414fb2eb116b2c66260bd855cb30d854dc04e
YUNA9627 <yuna2175@naver.com>
2024-11-16 16:30:57 +0900

    📝 docs: 공지사항 수정 페이지 작성

---
+++ admin/inquiry/notice_modify.php
@@ -0,0 +1,93 @@
+<?php
+include_once($_SERVER['DOCUMENT_ROOT'] . '/code_even/admin/inc/header.php');
+
+$ntid = $_GET['ntid'];
+$sql = "SELECT notice.*, user.username, user.userid FROM notice JOIN user ON notice.uid = user.uid WHERE ntid = $ntid";
+$result = $mysqli->query($sql);
+$data = $result->fetch_object();
+
+?>
+
+<div class="container">
+  <h2>문의게시판 관리</h2>
+  <div class="content_bar">
+    <h3>전체 공지사항 수정</h3>
+  </div>
+
+  <form action="notice_modify_ok.php" method="POST" enctype="multipart/form-data">
+  <input type="hidden" name="username" value="<?= $data->username ?>">
+  <input type="hidden" name="userid" value="<?= $data->userid; ?>">
+  <input type="hidden" name="ntid" value="<?= $data->ntid; ?>">
+    <table class="table details_table">
+      <colgroup>
+        <col style="width:160px">
+        <col style="width:516px">
+      </colgroup>
+      <thead class="thead-hidden">
+        <tr>
+          <th scope="col">구분</th>
+          <th scope="col">내용</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">이름(아이디)</th>
+          <td><?=$data->username;?>(<?=$data->userid;?>)</td>
+          <th scope="row">상태 <b>*</b></th>
+          <td class="d-flex gap-3">
+            <div class="form-check">
+              <input class="form-check-input" type="radio" name="status" id="status" value="on"
+                <?= ($data->status === 'on') ? 'checked' : ''; ?>
+              >
+              <label class="form-check-label" for="status">
+                노출
+              </label>
+            </div>
+            <div class="form-check">
+              <input class=" form-check-input" type="radio" name="status" id="status" value="off"
+                <?= ($data->status === 'off') ? 'checked' : ''; ?>
+              >
+              <label class="form-check-label" for="status">
+                숨김
+              </label>
+            </div>
+          </td>
+        </tr>
+        <tr class="none">
+          <th scope="row">제목 <b>*</b></th>
+          <td colspan="3">
+            <div>
+              <input type="text" name="title" class="form-control w-75" id="title" placeholder="제목을 입력해주세요." value="<?=$data->title;?>">
+            </div>
+          </td>
+        </tr>
+        <tr class="none">
+          <td colspan="3">
+            <div>
+              <textarea name="content" id="content" class="form-control" ><?=$data->content;?></textarea>
+            </div>
+          </td>
+        </tr>
+        <tr class="none">
+          <th scope="row">파일 등록 <b>*</b></th>
+          <td>
+            <input type="file" class="form-control" id="inputGroupFile02" class="w-50">
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <div class="custom-hr"></div>
+    <div class="d-flex justify-content-end gap-2">
+      <a href="http://<?= $_SERVER['HTTP_HOST']; ?>/code_even/admin/inquiry/notice.php" type="button"
+        class="btn btn-outline-danger">취소</a>
+      <button type="submit" class="btn btn-secondary">등록</button>
+    </div>
+  </form>
+
+</div>
+
+</div>
+
+<?php
+include_once($_SERVER['DOCUMENT_ROOT'] . '/code_even/admin/inc/footer.php');
+?>
