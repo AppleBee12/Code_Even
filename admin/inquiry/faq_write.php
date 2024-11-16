2b277bc8344fa8f694f5a31d2b226033565f8796
YUNA9627 <yuna2175@naver.com>
2024-11-17 02:28:58 +0900

    📝 docs: FAQ 통합 작성 페이지 작성

---
+++ admin/inquiry/faq_write.php
@@ -0,0 +1,105 @@
+<?php
+$title = "수강생 관리";
+include_once($_SERVER['DOCUMENT_ROOT'] . '/code_even/admin/inc/header.php');
+
+// $selected_target = isset($_POST['target']) ? $_POST['target'] : '';
+
+$target = $_GET['target'];
+
+$sql = "SELECT userid, username FROM user";
+$result = $mysqli->query($sql);
+$data = $result->fetch_object();
+
+?>
+
+<div class="container">
+  <h2>FAQ</h2>
+  <div class="content_bar">
+    <h3>FAQ 작성</h3>
+  </div>
+
+  <form action="faq_write_ok.php" method="POST" enctype="multipart/form-data">
+    <input type="hidden" name="username" value="<?= $data->username; ?>">
+    <input type="hidden" name="userid" value="<?= $data->userid; ?>">
+    <table class="table details_table">
+      <colgroup>
+        <col style="width:160px">
+        <col style="width:516px">
+        <col style="width:160px">
+        <col style="width:516px">
+      </colgroup>
+      <thead class="thead-hidden">
+        <tr>
+          <th scope="col">구분</th>
+          <th scope="col">내용</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">유형</th>
+          <td>
+            <select class="form-select w-50" aria-label="Default select example" name="target" id="target" disabled>
+              <option value="student" <?php if ($target === "student") echo "selected"; ?>>수강생</option>
+              <option value="teacher" <?php if ($target === "teacher") echo "selected"; ?>>강사</option>
+            </select>
+          </td>
+          <th scope="row">분류 <b>*</b></th>
+          <td>
+            <select class="form-select w-50" aria-label="Default select example" name="category">
+              <option value="">분류 선택</option>
+              <option value="1">결제/환불</option>
+              <option value="2">강의</option>
+              <option value="3">쿠폰</option>
+              <option value="4">가입/탈퇴</option>
+              <option value="5">기타</option>
+              <option value="6">수료</option>
+              <option value="7">정산</option>
+              <option value="8">강사</option>
+            </select>
+          </td>
+        </tr>
+        <tr>
+            <th scope="row">이름(아이디)</th>
+            <td><?= $data->username; ?>(<?= $data->userid; ?>)</td>
+            <th scope="row">상태 <b>*</b></th>
+            <td class="d-flex gap-3">
+              <div class="form-check">
+                <input class="form-check-input" type="radio" name="status" id="status" value="on">
+                <label class="form-check-label" for="status">
+                  노출
+                </label>
+              </div>
+              <div class="form-check">
+                <input class=" form-check-input" type="radio" name="status" id="status" value="off" checked>
+                <label class="form-check-label" for="status">
+                  숨김
+                </label>
+              </div>
+            </td>
+          </tr>
+        <tr class="none">
+          <th scope="row">제목 <b>*</b></th>
+          <td colspan="3">
+            <div>
+              <input type="text" name="title" class="form-control w-75" id="title" placeholder="제목을 입력해주세요.">
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <textarea name="content" id="" class="form-control w-75"></textarea>
+    <input type="file" class="form-control" id="inputGroupFile02" class="w-50">
+    <div class="custom-hr"></div>
+  
+    <div class="d-flex justify-content-end gap-2">
+      <a href="http://<?= $_SERVER['HTTP_HOST']; ?>/code_even/admin/inquiry/student_faq.php"
+        class="btn btn-outline-danger">취소</a>
+      <button type="submit" class="btn btn-secondary">등록</button>
+    </div>
+  </form>
+
+</div>
+
+<?php
+include_once($_SERVER['DOCUMENT_ROOT'] . '/code_even/admin/inc/footer.php');
+?>
