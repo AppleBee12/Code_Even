9fce73a9cd56a6bf1196072965783738b808bb53
YUNA9627 <yuna2175@naver.com>
2024-11-16 12:19:57 +0900

    📝 docs: 공지사항 목록 페이지 sql 작성

--- admin/inquiry/notice.php
+++ admin/inquiry/notice.php
@@ -1,7 +1,15 @@
 <?php
 include_once($_SERVER['DOCUMENT_ROOT'] . '/code_even/admin/inc/header.php');
-?>
 
+$sql = "SELECT notice.*, user.username, user.userid FROM notice JOIN user ON notice.uid = user.uid";
+$result = $mysqli->query($sql);
+
+$dataArr = [];
+while($data = $result->fetch_object()){
+  $dataArr[] = $data;
+}
+
+?>
 
 <div class="container">
   <h2>전체 공지사항</h2>
@@ -17,13 +25,9 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/code_even/admin/inc/header.php');
     </div>
   </form>
 
-  <form action="" method="">
     <table class="table list_table">
       <thead>
         <tr>
-          <th scope="col">
-            <input class="form-check-input" type="checkbox" value="" id="allCheck">
-          </th>
           <th scope="col">번호</th>
           <th scope="col">아이디</th>
           <th scope="col">이름</th>
@@ -35,52 +39,78 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/code_even/admin/inc/header.php');
         </tr>
       </thead>
       <tbody>
+        <?php   
+            if(isset($dataArr)){
+                foreach($dataArr as $no){                   
+            ?>
         <tr>
-          <th scope="row">
-            <input class="form-check-input itemCheckbox" type="checkbox" value="">
-          </th>
-          <td>2</td>
-          <td>admin</td>
-          <td>관리자</td>
-          <td><a href="http://<?= $_SERVER['HTTP_HOST']; ?>/code_even/admin/inquiry/notice_details.php"
-              class="underline">[공지] 결제요청 가이드라인 안내</a></td>
-          <td>9,999</td>
-          <td>2024/10/31</td>
+          <td><?=$no->ntid;?></td>
+          <td><?=$no->userid;?></td>
+          <td><?=$no->username;?></td>
+          <td><a href="http://<?= $_SERVER['HTTP_HOST']; ?>/code_even/admin/inquiry/notice_details.php?ntid=<?=$no->ntid;?>"
+              class="underline"><?=$no->title;?></a></td>
+          <td><?=$no->view;?></td>
+          <td><?=$no->regdate;?></td>
           <td>
-            <span class="badge text-bg-success">노출</span>
+            <!-- <span class="badge text-bg-success">노출</span> -->
+            <?php
+              $class = '';
+              $text = '';
+              if ($no->status == 'on') {
+                // status가 'on'일 경우
+                $class = 'text-bg-success';  // 노출 상태에 맞는 클래스
+                $text = '노출';  // 노출 글씨
+              } else {
+                // status가 'off'일 경우
+                $class = 'text-bg-light';  // 숨김 상태에 맞는 클래스
+                $text = '숨김';  // 숨김 글씨
+              }
+              echo "<span class='badge $class'>$text</span>";
+            ?>
           </td>
           <td>
-            <a href="">
+            <a href="http://<?= $_SERVER['HTTP_HOST']; ?>/code_even/admin/inquiry/notice_details.php?ntid=<?=$no->ntid;?>">
               <i class="bi bi-pencil-fill"></i>
             </a>
-            <a href="">
+            <a href="http://<?= $_SERVER['HTTP_HOST']; ?>/code_even/admin/inquiry/notice_delete.php?ntid=<?=$no->ntid;?>">
               <i class="bi bi-trash-fill"></i>
             </a>
           </td>
         </tr>
-        <tr>
-          <th scope="row">
-            <input class="form-check-input itemCheckbox" type="checkbox" value="">
-          </th>
-          <td>1</td>
-          <td>admin</td>
-          <td>관리자</td>
-          <td><a href="http://<?= $_SERVER['HTTP_HOST']; ?>/code_even/admin/inquiry/notice_details.php"
-              class="underline">[공지] 결제요청 가이드라인 안내</a></td>
-          <td>9,999</td>
-          <td>2024/10/31</td>
-          <td>
-            <span class="badge text-bg-light">숨김</span>
-          </td>
-          <td>
-            <a href="">
-              <i class="bi bi-pencil-fill"></i>
-            </a>
-            <a href="">
-              <i class="bi bi-trash-fill"></i>
-            </a>
-          </td>
-        </tr>
+          <?php   
+              }   
+            }              
+            ?>
+        <?php
+
+        // 게시글 개수 구하기
+        $page_sql = "SELECT COUNT(*) AS cnt FROM notice";
+        $page_result = $mysqli->query($page_sql);
+        $page_data = $page_result->fetch_assoc();
+        $row_num = $page_data['cnt'];
+
+        // 페이지네이션
+        if(isset($_GET['page'])){
+          $page = $_GET['page'];
+        }else {
+          $page = 1;
+        }
+
+        $list = 10;
+        $start_num = ($page - 1)*$list;
+        $block_ct = 5;
+        $block_num = ceil($page/$block_ct); // ceil 무조건 올리기
+        // 총 168개 10개씩 17개
+        $block_start = (($block_num - 1) * $block_ct) + 1;
+        $block_end = $block_start + $block_ct - 1;
+
+        $total_page = ceil($row_num / $list); // 총 168개 10개씩, 17
+        $total_block = ceil($total_page/$block_ct);
+        if($block_end > $total_page) $block_end = $total_page;
+
+        $sql = "SELECT * FROM notice ORDER BY ntid DESC LIMIT $start_num, $list";
+        $result = $mysqli->query($sql);
+        ?>
       </tbody>
     </table>
 
@@ -89,26 +119,44 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/code_even/admin/inc/header.php');
         class="btn btn-outline-secondary">상태일괄수정</button>
       <a href="http://<?= $_SERVER['HTTP_HOST']; ?>/code_even/admin/inquiry/notice_write.php"
         class="btn btn-secondary">등록</a>
-      <button type="button" class="btn btn-danger">삭제</button>
     </div>
 </div>
 
 <!-- //Pagination -->
 <div class="list_pagination" aria-label="Page navigation example">
   <ul class="pagination d-flex justify-content-center">
+    <?php
+      if($block_num > 1){
+        $prev = $block_start - $block_ct; 
+    ?>
     <li class="page-item">
       <a class="page-link" href="" aria-label="Previous">
         <i class="bi bi-chevron-left"></i>
       </a>
     </li>
-    <li class="page-item active"><a class="page-link" href="">1</a></li>
-    <li class="page-item"><a class="page-link" href="">2</a></li>
-    <li class="page-item"><a class="page-link" href="">3</a></li>
+    <?php
+      }
+    ?>
+    <?php
+      for($i = $block_start; $i <= $block_end; $i++){
+        // if($page == $i){$active = 'active';}
+        // else {$active = '';}
+        $page == $i ? $active = 'active' : $active = ''
+    ?>
+    <li class="page-item active"><a class="page-link" href=""><?= $i; ?></a></li>
+    <?php
+      }
+      $next = $block_end + 1;
+      if($total_block > $block_num){
+    ?>
     <li class="page-item">
       <a class="page-link" href="" aria-label="Next">
         <i class="bi bi-chevron-right"></i>
       </a>
     </li>
+    <?php
+      }
+    ?>
   </ul>
 </div>
 
@@ -166,8 +214,10 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/code_even/admin/inc/header.php');
     </div>
   </div>
 </div>
-</form>
 
+<script>
+
+</script>
 
 <?php
 include_once($_SERVER['DOCUMENT_ROOT'] . '/code_even/admin/inc/footer.php');
