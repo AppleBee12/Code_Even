1d305cbcea8757194e1b88b9418df90ebf07768d
YUNA9627 <yuna2175@naver.com>
2024-11-18 15:44:10 +0900

    🔨 chore: 주석 처리

--- admin/inquiry/notice_write_ok.php
+++ admin/inquiry/notice_write_ok.php
@@ -8,30 +8,28 @@ $title = $_POST['title'];
 $content = $_POST['content'];
 $status = $_POST['status'];
 
-print_r($_FILES['file']);
-
 $notice_sql = "
     INSERT INTO notice (uid, title, content, status)
-    SELECT uid, '$title', '$content', '$status'
+    SELECT uid, '$title', '$content', '$status'  
     FROM user
     WHERE username = '$username' AND userid = '$userid'
 ";
 
 $user_result = $mysqli->query($notice_sql);
 
-if ($user_result === true) {
-  echo
-    "<script>
-    confirm('글을 등록하시겠습니까?');
-    alert('등록이 완료되었습니다.');
-    location.href='notice.php';
-  </script>";
-} else {
-  echo
-    "<script>
-    alert('글쓰기 실패');
-    history.back();
-  </script>";
-}
+// if ($user_result === true) {
+//   echo
+//     "<script>
+//     confirm('글을 등록하시겠습니까?');
+//     alert('등록이 완료되었습니다.');
+//     location.href='notice.php';
+//   </script>";
+// } else {
+//   echo
+//     "<script>
+//     alert('글쓰기 실패');
+//     history.back();
+//   </script>";
+// }
 
 ?>
