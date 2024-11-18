01dde21430b1fd115c8385dbc97ed4cfdc63688a
AppleBee12 <mieldeluna@naver.com>
2024-11-18 15:16:13 +0900

    ✨ feat: 검색창 기능 및 페이지네이션 기능 추가

--- admin/community/counsel.php
+++ admin/community/counsel.php
@@ -1,6 +1,47 @@
 <?php
 $title = "고민 상담";
 include_once($_SERVER['DOCUMENT_ROOT'] . '/CODE_EVEN/admin/inc/header.php');
+
+// 게시글 개수 구하기
+$keywords = isset($_GET['keywords']) ? $mysqli->real_escape_string($_GET['keywords']) : '';
+$where_clause = '';
+
+if ($keywords) {
+  $where_clause = "WHERE counsel.title LIKE '%$keywords%' OR counsel.contents LIKE '%$keywords%' OR user.usernick LIKE '%$keywords%' ";
+}
+
+$page_sql = "SELECT COUNT(*) AS cnt FROM counsel JOIN user ON counsel.uid = user.uid $where_clause";
+$page_result = $mysqli->query($page_sql);
+$page_data = $page_result->fetch_assoc();
+$row_num = $page_data['cnt'];
+
+// 페이지네이션
+$page = isset($_GET['page']) ? $_GET['page'] : 1;
+$list = 10;
+$start_num = ($page - 1) * $list;
+$block_ct = 5;
+$block_num = ceil($page / $block_ct);
+$block_start = (($block_num - 1) * $block_ct) + 1;
+$block_end = $block_start + $block_ct - 1;
+
+$total_page = ceil($row_num / $list);
+$total_block = ceil($total_page / $block_ct);
+if ($block_end > $total_page) {
+  $block_end = $total_page;
+}
+
+$sql = "SELECT counsel.*, user.uid, user.usernick 
+        FROM counsel 
+        JOIN user ON counsel.uid = user.uid 
+        $where_clause 
+        ORDER BY counsel.post_id DESC 
+        LIMIT $start_num, $list";
+$result = $mysqli->query($sql);
+
+$dataArr = [];
+while ($data = $result->fetch_object()) {
+  $dataArr[] = $data;
+}
 ?>
 
 
@@ -12,7 +53,7 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/CODE_EVEN/admin/inc/header.php');
   <form action="" id="search_form" class="row justify-content-end">
     <div class="col-lg-3">
       <div class="input-group mb-3">
-        <input type="text" class="form-control" placeholder="검색어를 입력해주세요" aria-label="counsel" aria-describedby="basic-addon2">
+      <input type="text" class="form-control" placeholder="검색어를 입력하세요." name="keywords" value="<?= htmlspecialchars($keywords); ?>">
         <button type="button" class="btn btn-secondary">
           <i class="bi bi-search"></i>
         </button>
@@ -24,6 +65,8 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/CODE_EVEN/admin/inc/header.php');
 
   <form action="">
     <table class="table list_table">
+
+      
       <thead>
         <tr>
           <th scope="col">번호</th>
@@ -39,16 +82,20 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/CODE_EVEN/admin/inc/header.php');
         </tr>
       </thead>
       <tbody>
+      <?php   
+        if ($dataArr) {
+          foreach ($dataArr as $counsel) {
+      ?>
         <tr>
-          <th scope="row">152</th>
-          <td>Mark</td>
-          <td><a href="#">뭐부터 공부해야 할지..</a></td>
-          <td><a href="#">백엔드 개발자가 되고픈 30대..</a></td>
-          <td><span class="badge text-bg-light">미해결</span></td>
-          <td>0 <b>개</b></td>
-          <td>0<b>개</b></td>
-          <td>6<b>개</b></td>
-          <td>2024/11/17</td>
+          <th scope="row"><?=$counsel->post_id?>152</th>
+          <td><?=$counsel->usernick?></td>
+          <td><a href="#"><?=$counsel->titles?></a></td>
+          <td><a href="#"><?=$counsel->contents?></a></td>
+          <td><span class="badge text-bg-light"><?=$counsel->status?></span></td>
+          <td><?=$counsel->likes?><b>개</b></td>
+          <td><?=$counsel->comments?><b>개</b></td>
+          <td><?=$counsel->hit?><b>개</b></td>
+          <td><?=$counsel->regdate?></td>
           <td class="edit_col">
             <a href="">
               <i class="bi bi-pencil-fill"></i>
@@ -58,90 +105,56 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/CODE_EVEN/admin/inc/header.php');
             </a>
           </td>
         </tr>
-        <tr>
-          <th scope="row">151</th>
-          <td>Jacob</td>
-          <td><a href="#">c언어 기출문제의 변형</a></td>
-          <td><a href="#">앞 뒤가 똑같은 숫자 한번만...</a></td>
-          <td><span class="badge text-bg-success">해결</span></td>
-          <td>0 <b>개</b></td>
-          <td>2<b>개</b></td>
-          <td>5<b>개</b></td>
-          <td>2024/11/17</td>
-          <td class="edit_col">
-            <a href="">
-              <i class="bi bi-pencil-fill"></i>
-            </a>
-            <a href="">
-              <i class="bi bi-trash-fill"></i>
-            </a>
-          </td>
-        </tr>
-        <tr>
-          <th scope="row">150</th>
-          <td>Larry</td>
-          <td><a href="#">index.html 500오류..</a></td>
-          <td><a href="#">에러뜨고 안되여ㅠㅠ<!DOCT..</a></td>
-          <td><span class="badge text-bg-light">미해결</span></td>
-          <td>0 <b>개</b></td>
-          <td>0 <b>개</b></td>
-          <td>11<b>개</b></td>
-          <td>2024/11/17</td>
-          <td class="edit_col">
-            <a href="">
-              <i class="bi bi-pencil-fill"></i>
-            </a>
-            <a href="">
-              <i class="bi bi-trash-fill"></i>
-            </a>
-          </td>
-        </tr>
-        <tr>
-          <th scope="row">149</th>
-          <td>Larry</td>
-          <td><a href="#">1-D런타임에러가 ..</a></td>
-          <td><a href="#">vscode에선 잘 실행되는데 제..</a></td>
-          <td><span class="badge text-bg-success">해결</span></td>
-          <td>2 <b>개</b></td>
-          <td>2 <b>개</b></td>
-          <td>14<b>개</b></td>
-          <td>2024/11/17</td>
-          <td class="edit_col">
-            <a href="">
-              <i class="bi bi-pencil-fill"></i>
-            </a>
-            <a href="">
-              <i class="bi bi-trash-fill"></i>
-            </a>
-          </td>
-        </tr>
-        
+        <?php   
+          }   
+        } else {
+          echo "<tr><td colspan='8'>검색 결과가 없습니다.</td></tr>";
+        }
+      ?>
+      </tbody>
+
 
     </table>
     <!-- //table -->
     <button type="button" class="btn btn-outline-secondary ms-auto d-block">일괄수정</button>
   </form>
 
-
-
-  <div class="list_pagination" aria-label="Page navigation example">
-    <ul class="pagination d-flex justify-content-center">
-      <li class="page-item">
-        <a class="page-link" href="" aria-label="Previous">
-          <i class="bi bi-chevron-left"></i>
-        </a>
-      </li>
-      <li class="page-item active"><a class="page-link" href="">1</a></li>
-      <li class="page-item"><a class="page-link" href="">2</a></li>
-      <li class="page-item"><a class="page-link" href="">3</a></li>
-      <li class="page-item">
-        <a class="page-link" href="" aria-label="Next">
-          <i class="bi bi-chevron-right"></i>
-        </a>
-      </li>
-    </ul>
-  </div>
-  <!-- //Pagination -->
+<!-- //Pagination -->
+<div class="list_pagination" aria-label="Page navigation example">
+  <ul class="pagination d-flex justify-content-center">
+    <?php
+      $previous = $block_start - $block_ct;
+      if ($previous < 1) $previous = 1;
+      if ($block_num > 1) { 
+    ?>
+    <li class="page-item">
+      <a class="page-link" href="counsel.php?page=<?= $previous; ?>" aria-label="Previous">
+        <i class="bi bi-chevron-left"></i>
+      </a>
+    </li>
+    <?php
+      }
+    ?>
+    <?php
+      for ($i = $block_start; $i <= $block_end; $i++) {
+        $active = ($page == $i) ? 'active' : '';
+    ?>
+    <li class="page-item <?= $active; ?>"><a class="page-link" href="counsel.php?page=<?= $i; ?>"><?= $i; ?></a></li>
+    <?php
+      }
+      $next = $block_end + 1;
+      if($total_block > $block_num){
+    ?>
+    <li class="page-item">
+      <a class="page-link" href="counsel.php?page=<?= $next; ?>" aria-label="Next">
+        <i class="bi bi-chevron-right"></i>
+      </a>
+    </li>
+    <?php
+      }
+    ?>
+  </ul>
+</div>
 </div>
 
 
