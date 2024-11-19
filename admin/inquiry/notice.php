b6e3aab7562534fb1eac02d2561de1aa6d500e20
YUNA9627 <yuna2175@naver.com>
2024-11-19 16:56:32 +0900

    📝 docs:  웹접근성을 위한 a태그 button으로 변경 및 form태그로 감쌈

--- admin/inquiry/notice.php
+++ admin/inquiry/notice.php
@@ -58,63 +58,62 @@ while ($data = $result->fetch_object()) {
     </div>
   </form>
 
-  <table class="table list_table">
-    <thead>
-      <tr>
-        <th scope="col">번호</th>
-        <th scope="col">아이디</th>
-        <th scope="col">이름</th>
-        <th scope="col">제목</th>
-        <th scope="col">조회수</th>
-        <th scope="col">등록일</th>
-        <th scope="col">상태</th>
-        <th scope="col">관리</th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php
-      if ($dataArr) {
-        foreach ($dataArr as $no) {
-          ?>
-          <tr>
-            <td><?= $no->ntid; ?></td>
-            <td><?= $no->userid; ?></td>
-            <td><?= $no->username; ?></td>
-            <td><a
-                href="http://<?= $_SERVER['HTTP_HOST']; ?>/code_even/admin/inquiry/notice_modify.php?ntid=<?= $no->ntid; ?>"
-                class="underline"><?= $no->title; ?></a></td>
-            <td><?= $no->view; ?></td>
-            <td><?= $no->regdate; ?></td>
-            <td>
-              <?php
-              $class = $no->status == 'on' ? 'text-bg-success' : 'text-bg-light';
-              $text = $no->status == 'on' ? '노출' : '숨김';
-              echo "<span class='badge $class'>$text</span>";
-              ?>
-            </td>
-            <td class="edit_col">
-              <a
-                href="http://<?= $_SERVER['HTTP_HOST']; ?>/code_even/admin/inquiry/notice_modify.php?ntid=<?= $no->ntid; ?>">
-                <i class="bi bi-pencil-fill"></i>
-              </a>
-              <a
-                href="http://<?= $_SERVER['HTTP_HOST']; ?>/code_even/admin/inquiry/notice_delete.php?ntid=<?= $no->ntid; ?>">
-                <i class="bi bi-trash-fill"></i>
-              </a>
-            </td>
-          </tr>
-          <?php
+  <form action="notice_write.php">
+    <table class="table list_table">
+      <thead>
+        <tr>
+          <th scope="col">번호</th>
+          <th scope="col">아이디</th>
+          <th scope="col">이름</th>
+          <th scope="col">제목</th>
+          <th scope="col">조회수</th>
+          <th scope="col">등록일</th>
+          <th scope="col">상태</th>
+          <th scope="col">관리</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        if ($dataArr) {
+          foreach ($dataArr as $no) {
+            ?>
+            <tr>
+              <td><?= $no->ntid; ?></td>
+              <td><?= $no->userid; ?></td>
+              <td><?= $no->username; ?></td>
+              <td><a
+                  href="http://<?= $_SERVER['HTTP_HOST']; ?>/code_even/admin/inquiry/notice_modify.php?ntid=<?= $no->ntid; ?>"
+                  class="underline"><?= $no->title; ?></a></td>
+              <td><?= $no->view; ?></td>
+              <td><?= $no->regdate; ?></td>
+              <td>
+                <?php
+                $class = $no->status == 'on' ? 'text-bg-success' : 'text-bg-light';
+                $text = $no->status == 'on' ? '노출' : '숨김';
+                echo "<span class='badge $class'>$text</span>";
+                ?>
+              </td>
+              <td class="edit_col">
+                <a
+                  href="http://<?= $_SERVER['HTTP_HOST']; ?>/code_even/admin/inquiry/notice_modify.php?ntid=<?= $no->ntid; ?>">
+                  <i class="bi bi-pencil-fill"></i>
+                </a>
+                <a
+                  href="http://<?= $_SERVER['HTTP_HOST']; ?>/code_even/admin/inquiry/notice_delete.php?ntid=<?= $no->ntid; ?>">
+                  <i class="bi bi-trash-fill"></i>
+                </a>
+              </td>
+            </tr>
+            <?php
+          }
+        } else {
+          echo "<tr><td colspan='10'>검색 결과가 없습니다.</td></tr>";
         }
-      } else {
-        echo "<tr><td colspan='10'>검색 결과가 없습니다.</td></tr>";
-      }
-      ?>
-    </tbody>
-  </table>
-
-  <div class="d-flex justify-content-end gap-2">
-    <a href="http://<?= $_SERVER['HTTP_HOST']; ?>/code_even/admin/inquiry/notice_write.php" class="btn btn-secondary">등록</a>
-  </div>
+        ?>
+      </tbody>
+    </table>
+    <button type="submit" class="btn btn-secondary ms-auto d-block">등록</button>
+  </form>
 </div>
 
 <!-- //Pagination -->
