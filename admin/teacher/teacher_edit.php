fb513779d0dec82ca257ada8a8e443c680f11667
eunjin6-6 <dmswls616@gmail.com>
2024-11-19 22:04:32 +0900

    ♻️ refactor: 웹접근성 피드백 수정, 테이블 2개 분리

--- admin/teacher/teacher_edit.php
+++ admin/teacher/teacher_edit.php
@@ -18,12 +18,10 @@
   }
 ?>
 
-
-
 <div class="container">
   <h2>강사 프로필 수정</h2>
   <div class="content_bar">
-    <h3>강사 상세정보</h3>
+    <h3>강사기본정보</h3>
   </div>
   <form action="teacher_edit_ok.php" id="teacher_save" method="POST" enctype="multipart/form-data">
     <input type="hidden" name="tcid" value="<?= $data->tcid; ?>">
@@ -50,40 +48,8 @@
           <td>
             <input type="text" class="form-control" id="exampleFormControlInput1" name="tc_name" value="<?= $data->tc_name; ?>" required>
           </td>
-          <th scope="row">링크</th>
-          <td>
-            <input type="text" class="form-control" id="exampleFormControlInput1" name="tc_url" value="<?= $data->tc_url; ?>" placeholder="https://">
-          </td>
-        </tr>
-        <tr>
-          <th scope="row">아이디 <b>*</b></th>
-          <td>
-            <input type="text" class="form-control" id="exampleFormControlInput1" name="tc_userid" value="<?= $data->tc_userid; ?>">
-          </td>
-          <th scope="row">은행명</th>
-          <td>
-            <input type="text" class="form-control" id="exampleFormControlInput1" name="tc_bank" value="<?= $data->tc_bank; ?>">
-          </td>
-        </tr>
-        <tr>
-          <th scope="row">연락처 <b>*</b></th>
-          <td>
-            <input type="text" class="form-control" id="tc_phone" name="tc_userphone" value="<?= $data->tc_userphone; ?>">
-          </td>
-          <th scope="row">계좌번호</th>
-          <td>
-            <input type="text" class="form-control" id="exampleFormControlInput1" name="tc_account"  value="<?= $data->tc_account; ?>">
-          </td>
-        </tr>
-        <tr>
-          <th scope="row">이메일 <b>*</b></th>
-          <td colspan="3">
-            <input type="text" class="form-control" id="exampleFormControlInput1" name="tc_email"  value="<?= $data->tc_email; ?>">
-          </td>
-        </tr>
-        <tr>
           <th scope="row">대표분야 <b>*</b></th>
-          <td colspan="3">
+          <td>
             <select class="form-select" name="tc_cate" aria-label="대표분야">
               <?php foreach($categories as $category): ?>
                 <option value="<?= $category->cgid;?>" <?= $data->tc_cate == $category->cgid ? 'selected' : ''; ?>>
@@ -92,55 +58,36 @@
               <?php endforeach; ?>
             </select>
           </td>
+          
         </tr>
         <tr>
-          <td colspan="4">
-            <hr>
-          </td>
-        </tr>
-
-
-        <tr>
-          <th scope="row">승인상태</th>
+          <th scope="row"><label for="tc_userid">아이디 <b>*</b></label></th>
           <td>
-            <select class="form-select tc_status" aria-label="승인여부" name="tc_ok" id="tc_ok">
-                <option value="-1" <?php if($data->tc_ok == -1){echo 'selected';}?>>승인거절</option>
-                <option value="0" <?php if($data->tc_ok == 0){echo 'selected';}?>>심사중</option>
-                <option value="1" <?php if($data->tc_ok == 1){echo 'selected';}?>>승인완료</option>
-            </select>
-            <!-- 드롭다운메뉴로 변경, 확정 시 삭제하기
-            <div class="form-check form-check-inline">
-              <input class="form-check-input" type="radio" name="tc_ok" id="inlineRadio1" value="0" <?php if($data->tc_ok == 0){echo 'checked';}?>>
-              <label class="form-check-label" for="inlineRadio1">심사중</label>
-            </div>
-            <div class="form-check form-check-inline">
-              <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="1" <?php if($data->tc_ok == 1){echo 'checked';}?>> 
-              <label class="form-check-label" for="inlineRadio2">승인완료</label>
-            </div>
-            <div class="form-check form-check-inline">
-              <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="-1" <?php if($data->tc_ok == -1){echo 'checked';}?>> 
-              <label class="form-check-label" for="inlineRadio3">승인거절</label>
-            </div> -->
+            <input type="text" class="form-control" id="tc_userid" name="tc_userid" value="<?= $data->tc_userid; ?>">
           </td>
-        <th scope="row">강사전시옵션</th>
+          <th scope="row"><label for="tc_bank">은행명</label></th>
           <td>
-          <div class="form-check form-check-inline d-inline-block">
-                <input class="form-check-input" type="checkbox" <?php echo $data->isnew ? 'checked' : ''; ?> value="<?= $data->isnew ?>" name="isnew" id="isnew">
-                <label class="form-check-label" for="isnew">
-                  신규
-                </label>
-              </div>
-              <div class="form-check d-inline-block">
-                <input class="form-check-input" type="checkbox" <?php echo $data->isrecom ? 'checked' : ''; ?> value="<?= $data->isrecom ?>" name="isrecom" id="isrecom">
-                <label class="form-check-label" for="isrecom">
-                  추천
-                </label>
-              </div>
+            <input type="text" class="form-control" id="tc_bank" name="tc_bank" value="<?= $data->tc_bank; ?>">
           </td>
         </tr>
         <tr>
-          <td colspan="4">
-              <hr>
+          <th scope="row"><label for="tc_userphone">연락처 <b>*</b></label></th>
+          <td>
+          <input type="text" class="form-control" id="tc_userphone" name="tc_userphone" value="<?= $data->tc_userphone; ?>">
+          </td>
+          <th scope="row"><label for="tc_account">계좌번호</label></th>
+          <td>
+            <input type="text" class="form-control" id="tc_account" name="tc_account" value="<?= $data->tc_account; ?>">
+          </td>
+        </tr>
+        <tr>
+          <th scope="row"><label for="tc_email">이메일 <b>*</b></label></th>
+          <td>
+            <input type="text" class="form-control" id="tc_email" name="tc_email" value="<?= $data->tc_email; ?>">
+          </td>
+          <th scope="row"><label for="tc_url">링크</label></th>
+          <td>
+            <input type="text" class="form-control" id="tc_url" name="tc_url" value="<?= $data->tc_url; ?>" placeholder="https://">
           </td>
         </tr>
         <tr>
@@ -148,13 +95,55 @@
           <td colspan="3">
           <div id="summernote"><?= $data->tc_intro;?></div>
           </td>
-
         </tr>
-        
+        <tr>
+          <td colspan="4">
+              <hr>
+          </td>
+        </tr>
       </tbody>
     </table>
+    <div class="content_bar">
+      <h3>강사상태관리</h3>
+    </div>
+    <table class="table w-100 info_table">
+      <colgroup>
+        <col class="col-width-160">
+        <col class="col-width-516">
+        <col class="col-width-160">
+        <col class="col-width-516">
+      </colgroup>
+      <tbody>
+        <tr>
+          <th scope="row"><label for="tc_ok">승인상태 <b>*</b></label></th>
+          <td>
+            <select class="form-select tc_status" name="tc_ok" id="tc_ok" aria-label="승인상태 선택">
+              <option value="-1" <?php if($data->tc_ok == -1){echo 'selected';}?>>승인거절</option>
+              <option value="0" <?php if($data->tc_ok == 0){echo 'selected';}?>>심사중</option>
+              <option value="1" <?php if($data->tc_ok == 1){echo 'selected';}?>>승인완료</option>
+            </select>
+          </td>
+          <th scope="row"><label for="isnew">강사전시옵션</label></th>
+          <td>
+            <div class="form-check form-check-inline d-inline-block">
+              <input class="form-check-input" type="checkbox" <?php echo $data->isnew ? 'checked' : ''; ?> value="<?= $data->isnew ?>" name="isnew" id="isnew">
+              <label class="form-check-label" for="isnew">
+                신규
+              </label>
+            </div>
+            <div class="form-check d-inline-block">
+              <input class="form-check-input" type="checkbox" <?php echo $data->isrecom ? 'checked' : ''; ?> value="<?= $data->isrecom ?>" name="isrecom" id="isrecom">
+              <label class="form-check-label" for="isrecom">
+                추천
+              </label>
+            </div>
+          </td>
+        </tr>     
+      </tbody>
+    </table>
+
     <div class="d-flex justify-content-end gap-2">
-      <a href="teacher_list.php" type="button" class="btn btn-outline-danger">취소</a>
+      <a href="teacher_list.php" class="btn btn-outline-danger" role="button">취소</a>
       <button class="btn btn-outline-secondary">수정</button>
     </div>
   </form>
@@ -163,6 +152,7 @@
   <div id="profileToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
     <div class="toast-header">
       <strong class="me-auto">&#x1F525; 알림</strong>
+      <small>2초 후 종료</small>
       <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
     </div>
     <div class="toast-body">
@@ -171,7 +161,6 @@
   </div>
 </div>
 
-<script src="path/to/bootstrap.bundle.min.js"></script>
 <script>
   //토스트 알림창 제어
   $(document).ready(function() {
@@ -220,18 +209,11 @@
     }
   });
 
-
-
-
   $('#teacher_save').submit(function(e){
     //e.preventDefault();
     var markup = target.summernote('code');
     let content = encodeURIComponent(markup);
     $('#contents').val(markup);
-    
-    //var plainText = $('#summernote').summernote('code').replace(/<\/?[^>]+(>|$)/g, ""); // HTML 태그 제거
-    //$('#contents').val(plainText);
-    //console.log(markup); 
   });
 
 </script>
