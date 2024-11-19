7ba72792c58474a4a84e0da4893c0aced50b3ed1
AppleBee12 <mieldeluna@naver.com>
2024-11-20 02:38:07 +0900

    📝 docs: 강사용 프로필 데이터받아오기

--- admin/teacher_page/myprofile/teacher_details.php
+++ admin/teacher_page/myprofile/teacher_details.php
@@ -2,6 +2,24 @@
 $title = "강사";
 include_once($_SERVER['DOCUMENT_ROOT'] . '/code_even/admin/inc/dbcon.php');
 include_once($_SERVER['DOCUMENT_ROOT'] . '/code_even/admin/inc/header.php');
+
+
+
+
+$uid = $_SESSION['UID'];
+
+$sql = "SELECT * FROM teachers WHERE uid = $uid";
+$result = $mysqli->query($sql);
+$tc = $result->fetch_object();
+$tcid = $tc->tcid;
+
+$category_sql = "SELECT * FROM category WHERE code LIKE 'A%' ORDER BY cgid ASC";
+$category_result = $mysqli->query($category_sql);
+
+while($cate_data = $category_result->fetch_object()){
+    $categories[] = $cate_data;
+}
+
 ?>
 
 <div class="container">
@@ -10,9 +28,10 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/code_even/admin/inc/header.php');
     <h3>강사 상세정보</h3>
   </div>
 
-  <form action="">
+  <form action="teacher_edit_ok.php" id="teacher_save" method="POST" enctype="multipart/form-data">
+  <input type="hidden" name="tcid" value="<?= $tc->tcid; ?>">
     <div class="upload mt-5 mb-3">
-      <img src="https://picsum.photos/200" width=100 height=100 alt="">
+      <img src="http://<?= $_SERVER['HTTP_HOST'] . $tc->tc_thumbnail ?>" width=100 height=100 alt="강사 프로필 섬네일">
       <div class="round">
         <input type="file">
         <i class="bi bi-camera-fill"></i>
@@ -22,81 +41,75 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/code_even/admin/inc/header.php');
 
     <table class="table w-100 info_table">
       <colgroup>
-        <col width="160">
-        <col width="516">
-        <col width="160">
-        <col width="516">
+      <col class="col-width-160">
+        <col class="col-width-516">
+        <col class="col-width-160">
+        <col class="col-width-516">
       </colgroup>
       <tbody>
-        <?php
-        $sql = "SELECT * FROM user ORDER BY uid ";
-        $result = $mysqli->query($sql);
 
-        $user_id = $_SESSION['AUID'];
-        // print_r($result->fetch_assoc());
-        print_r($user_id);
-
-        ?>
         <tr>
           <th scope="row">
-            <label for="username">이름 <b>*</b></label>
+            <label for="tc_name">이름 <b>*</b></label>
           </th>
           <td>
-            <input type="text" id="username" name="username" class="form-control" placeholder="입력 필수 값 입니다." value="">
+            <input type="text" id="tc_name" name="tc_name" class="form-control" placeholder="입력 필수 값 입니다." value="<?= $tc->tc_name ?>" required>
           </td>
           <th>
           <label for="link">링크 <b>*</b></label>
           </th>
           <td>
-            <input type="text" id="link" name="link" class="form-control" id="exampleFormControlInput1" placeholder="냠냠">
+            <input type="text" id="link" name="link" class="form-control" id="exampleFormControlInput1" placeholder="입력 필수 값 입니다." required>
           </td>
         </tr>
         <tr>
           <th scope="row">
-            <label for="userid">아이디 <b>*</b></label>
+            <label for="tc_userid">아이디 <b>*</b></label>
           </th>
           <td>
-            <input type="text" id="userid" name="userid" class="form-control" placeholder="입력 필수 값 입니다." value="">
+            <input type="text" id="tc_userid" name="tc_userid" class="form-control" placeholder="입력 필수 값 입니다." value="<?= $tc->tc_userid ?>" required>
           </td>
           <th scope="row">
-            <label for="bank">은행명 <b>*</b></label>
+            <label for="tc_bank">은행명 <b>*</b></label>
           </th>
           <td>
-            <input type="text" id="bank" name="bank" class="form-control" placeholder="입력 필수 값 입니다." value="">
+            <input type="text" id="tc_bank" name="tc_bank" class="form-control" placeholder="입력 필수 값 입니다." value="<?= $tc->tc_bank ?>" required>
           </td>
 
         </tr>
         <tr>
           <th scope="row">
-            <label for="contact">연락처 <b>*</b></label>
+            <label for="tc_userphone">연락처 <b>*</b></label>
           </th>
           <td>
-            <input type="text" id="contact" name="contact" class="form-control" placeholder="입력 필수 값 입니다." value="">
+            <input type="text" id="tc_userphone" name="tc_userphone" class="form-control" placeholder="입력 필수 값 입니다." value="<?= $tc->tc_userphone ?>">
 
           <th scope="row">
-            <label for="bankaccount">계좌번호 <b>*</b></label>
+            <label for="tc_account">계좌번호 <b>*</b></label>
           </th>
           <td>
-            <input type="text" id="bankaccount" name="bankaccount" class="form-control" placeholder="입력 필수 값 입니다." value="">
+            <input type="text" id="tc_account" name="tc_account" class="form-control" placeholder="입력 필수 값 입니다." value="<?= $tc->tc_account ?>">
           </td>
         </tr>
         <tr>
           <th scope="row">
-            <label for="email">이메일 <b>*</b></label>
+            <label for="tc_email">이메일 <b>*</b></label>
           </th>
           <td colspan="3">
-            <input type="email" id="email" name="email" class="form-control w_512" placeholder="입력 필수 값 입니다." value="">
+            <input type="text" id="tc_email" name="tc_email" class="form-control" placeholder="입력 필수 값 입니다." value="<?= $tc->tc_account ?>">
           </td>
         </tr>
         <tr>
         <th scope="row">
-          <label for="cate">대표분야 <b>*</b></label>
+          <label for="tc_cate">대표분야 <b>*</b></label>
         </th>
         <td colspan="3">
-          <select class="form-select w_512" id="cate" name="cate" aria-label="대표 분야">
-            <option value="1" selected>웹개발</option>
-            <option value="2">클라우드</option>
-            <option value="3">보안</option>
+          <select class="form-select" id="tc_cate" name="tc_cate" aria-label="대표 분야">
+          <?php foreach($categories as $category): ?>
+                <option value="<?= $category->cgid;?>" <?= $tc->tc_cate == $category->cgid ? 'selected' : ''; ?>>
+                  <?= $category->name;?>
+                </option>
+              <?php endforeach; ?>
           </select>
         </td>
       </tr>
@@ -108,7 +121,7 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/code_even/admin/inc/header.php');
       <tr>
         <th scope="row">소개글 <b>*</b></th>
         <td colspan="3">
-        <div id="summernote"></div>
+        <div id="summernote"><?= $tc->tc_intro;?></div>
         </td>
 
       </tr>
