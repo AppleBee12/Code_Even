304d402b0ec27c9e42bcf0757d56d0a3b62cbcf7
chaerimjo-web <chaerim010710@gmail.com>
2024-11-21 00:51:56 +0900

    🚚 rename: members 로그인 및 회원가입 폼 생성

---
+++ members/login/login.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST']; ?>/code_even/admin/css/common.css">
+    <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST']; ?>/code_even/admin/css/main.css">
+  </head>
+
+  <style>
+    .findpw a{
+      /* display: none; */
+ 
+    }
+    .bgsz{
+      margin-top: 10%;
+    }
+    .images2{
+      /* background: #000; */
+      width: 300px;
+    }
+    .img-wrap{
+    }
+</style>
+
+  <body>
+    <div class="d-flex gap-5 align-items-center bgsz">
+      <div class="img-wrap  w-50 d-flex justify-content-center">
+        <div class="images1">
+          <img src="../images/sb_logo.png" alt="">
+        </div>
+      </div>
+      <div class="w-50 container d-flex justify-content-center align-items-center">
+        <div class="w-50 d-flex flex-column align-items-center">
+          <img src="../images/txt_logo.png" class="images2" alt="">
+          <h1 class="mt-5">로그인</h1>
+          <h2>관리자 로그인</h2>
+          <form action="login_ok.php" method="POST" class="w-100">
+            <label for="inputId" class="form-label mt-3">아이디</label>
+            <input type="text" id="inputId" class="form-control" placeholder="아이디를 입력하세요" name="userid" required>
+            
+            <label for="inputPassword" class="form-label mt-3">비밀번호</label>
+            <input type="password" id="inputPassword" class="form-control" placeholder="비밀번호를 입력하세요" name="userpw" required>
+            
+            <button class="btn btn-primary mt-3 w-100">로그인</button>
+            
+            <div class="mt-3 d-flex flex-columns justify-content-center gap-3">
+              <a href="#" class="link-body-emphasis ">아이디 찾기</a>
+              <a href="#" class="link-body-emphasis">비밀번호 찾기</a>
+              <a href="../signup/signup.php" class="link-body-emphasis text-decoration-underline">회원가입</a>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </body>
---
+++ members/signup/id_check.php
@@ -0,0 +1,32 @@
+<?php
+include_once($_SERVER['DOCUMENT_ROOT'] . '/CODE_EVEN/admin/inc/dbcon.php');
+
+$result_data = array('result' => 'error'); // 기본값 설정
+
+if (isset($_POST['userid'])) {
+    // 아이디 중복 체크
+    $userid = $_POST['userid'];
+    $sql = "SELECT COUNT(*) AS cnt FROM user WHERE userid = '$userid'";
+    $result = $mysqli->query($sql);
+    $data = $result->fetch_assoc();
+    if ($data['cnt'] > 0) {
+        $result_data = array('result' => 'error'); // 중복된 아이디
+    } else {
+        $result_data = array('result' => 'ok'); // 사용할 수 있는 아이디
+    }
+} else if (isset($_POST['usernick'])) {
+    // 닉네임 중복 체크
+    $usernick = $_POST['usernick'];
+    $sql = "SELECT COUNT(*) AS cnt FROM user WHERE usernick = '$usernick'";
+    $result = $mysqli->query($sql);
+    $data = $result->fetch_assoc();
+    if ($data['cnt'] > 0) {
+        $result_data = array('result' => 'error'); // 중복된 닉네임
+    } else {
+        $result_data = array('result' => 'ok'); // 사용할 수 있는 닉네임
+    }
+}
+
+echo json_encode($result_data); // 결과 반환
+$mysqli->close();
+?>
