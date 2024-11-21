bb4c7dde9f686621f72fd1853fd77aa4dedf55c8
AppleBee12 <mieldeluna@naver.com>
2024-11-21 12:54:32 +0900

    📝 docs: 관리자프로필 수정 구현완료

--- admin/myprofile/my_details.php
+++ admin/myprofile/my_details.php
@@ -2,6 +2,13 @@
 $title = "관리자";
 include_once($_SERVER['DOCUMENT_ROOT'] . '/code_even/admin/inc/dbcon.php');
 include_once($_SERVER['DOCUMENT_ROOT'] . '/code_even/admin/inc/header.php');
+
+
+$user_id = $_SESSION['AUID'];
+$sql = "SELECT uid, username, userphonenum, userid, useremail FROM user WHERE userid = '$user_id'";
+$result = $mysqli->query($sql);
+if ($result && $row = $result->fetch_assoc()) {
+
 ?>
 
 <div class="container">
@@ -10,24 +17,20 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/code_even/admin/inc/header.php');
     <h3>관리자 상세정보</h3>
   </div> 
 
-  <form action="">
+
   <div class="upload adminprofile mt-5 mb-3">
     <img src="http://<?= $_SERVER['HTTP_HOST']; ?>/code_even/admin/images/adminprofile.png"  alt="코드이븐 관리자 프로필">
   </div>
   <p class="text-center mb-5">프로필 이미지</p>
 
-  <table class="table info_table">
+  <form action="/code_even/admin/myprofile/my_details_ok.php" method="POST" enctype="multipart/form-data">
+    <input type="hidden" name="uid" value=" <?= $row['uid']; ?>">
+    <table class="table info_table">
     <tbody>
-    <?php
-        $user_id = $_SESSION['AUID'];
-        $sql = "SELECT uid, username, userphonenum, userid, useremail FROM user WHERE userid = '$user_id'";
-        $result = $mysqli->query($sql);
-        if ($result && $row = $result->fetch_assoc()) {
-          ?>
-
       <tr>
         <th scope="row"> 
-          <label for="username">이름 <b>*</b></label> </th>
+          <label for="username">이름 <b>*</b></label> 
+        </th>
         <td>
           <input type="text" id="username" name="username" class="form-control" placeholder="입력 필수 값 입니다." value="<?=$row['username']?>">
         </td>
@@ -65,13 +68,13 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/code_even/admin/inc/header.php');
         ?>  
     </tbody>
   </table>
-  </form>
   <p>* 상점 정보에 대한 수정은 좌측 하단 "상점 관리"에서 가능합니다. </p>
 
   <div class="d-flex justify-content-end gap-2">
-    <a href="javascript:history.back();" ><button class="btn btn-outline-danger">취소</button></a>
-    <a href="http://<?= $_SERVER['HTTP_HOST']; ?>/code_even/admin/index.php"><button class="btn btn-outline-secondary">수정</button></a>
+    <a href="/code_even/admin/myprofile/my_details_cancle.php" ><button type="button" class="btn btn-outline-danger">취소</button></a>
+    <button type="submit" class="btn btn-outline-secondary">수정</button>
   </div>
+  </form>
 </div>
 
 
---
+++ admin/myprofile/my_details_ok.php
@@ -0,0 +1,42 @@
+
+
+<?php
+include_once($_SERVER['DOCUMENT_ROOT'] . '/code_even/admin/inc/dbcon.php');
+
+// print_r($_POST);
+ $uid = $_POST['uid'];
+ $username = $_POST['username'];
+ $userphonenum = $_POST['userphonenum'];
+ $useremail = $_POST['useremail'];
+
+
+ $profile_sql = "
+ UPDATE user
+ SET 
+     username = '$username',
+     userphonenum = '$userphonenum',
+     useremail = '$useremail'
+ WHERE uid = '$uid'
+";
+
+$result = $mysqli->query($profile_sql);
+
+if ($result === true) {
+  echo
+  "<script>
+      if (confirm('프로필을 수정하시겠습니까?')) {
+          alert('등록이 완료되었습니다.');
+          location.href='/code_even/admin/index.php';
+      } else {
+          history.back();
+      }
+   </script>";
+} else {
+  echo
+    "<script>
+       alert('수정 실패');
+       history.back();
+     </script>";
+}
+
+?>
