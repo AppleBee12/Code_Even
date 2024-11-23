3af3a1ab007f56ad9d1799e36e987facecd5532f
eunjin6-6 <dmswls616@gmail.com>
2024-11-24 04:26:08 +0900

    📝 docs: datepicker 한글변경

--- admin/sales/monthly_sales.php
+++ admin/sales/monthly_sales.php
@@ -74,7 +74,7 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/CODE_EVEN/admin/inc/header.php');
   <form action="" id="search_form" class="row justify-content-end">
     <div class="col-lg-3">
       <!-- <input type="date" class="form-control" /> -->
-      <input type="text" id="datepicker"></p>
+      <input class="form-control" type="text" id="datepicker"></p>
     </div>
     <div class="col-lg-3">
     <div class="input-group mb-3">
@@ -236,9 +236,33 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/CODE_EVEN/admin/inc/header.php');
 
 </script>
 <script>
+$(document).ready(function(){               
+    $.datepicker.setDefaults({
+    closeText: "닫기",
+    currentText: "오늘",
+    prevText: '이전 달',
+    nextText: '다음 달',
+    monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+    monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+    dayNames: ['일', '월', '화', '수', '목', '금', '토'],
+    dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
+    dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+    weekHeader: "주",
+    yearSuffix: '년'
+    });    
+ });
+</script>
+
+<script>
+  
+
   $( function() {
-    $( "#datepicker" ).datepicker();
+    $( "#datepicker" ).datepicker({
+      dateFormat: "yy-mm-dd"
+    });
   } );
+
+
   </script>
 
 
