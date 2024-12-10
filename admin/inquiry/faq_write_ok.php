7134165b809025d99bd9cf62973419988ee40573
YUNA9627 <yuna2175@naver.com>
2024-12-10 18:16:25 +0900

    📝 docs: summernote 이미지 작성 sql 작성 완료

--- admin/inquiry/faq_write_ok.php
+++ admin/inquiry/faq_write_ok.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include_once($_SERVER['DOCUMENT_ROOT'] . '/CODE_EVEN/admin/inc/dbcon.php');
 
 // print_r($_POST);
@@ -9,6 +10,15 @@ $title = $_POST['title'];
 $content = $_POST['content'];
 $status = $_POST['status'];
 $target = $_POST['target'];
+$table_name = $_POST['table_name'];
+
+// 세션에서 이미지 URL 가져오기
+$imageUrl = isset($_SESSION['imageUrl']) ? $_SESSION['imageUrl'] : null;
+
+// 이미지 URL 값 확인하기
+// echo "<pre>";
+// var_dump($imageUrl); // imageUrl 값을 출력
+// echo "</pre>";
 
 $faq_sql = "
     INSERT INTO faq (uid, title, content, status, category, target)
@@ -16,10 +26,13 @@ $faq_sql = "
     FROM user
     WHERE username = '$username' AND userid = '$userid'
 ";
-
 $faq_result = $mysqli->query($faq_sql);
+$faq_id = $mysqli->insert_id;
 
-if ($faq_result === true) {
+$image_sql = "INSERT INTO summer_images (table_name, table_id, file_name) VALUES ('$table_name', '$faq_id', '$imageUrl')";
+$image_result = $mysqli->query($image_sql);
+
+if ($faq_result === true && $image_result === true) {
   $redirect_url = ($target === 'teacher') ? 'teacher_faq.php' : (($target === 'student') ? 'student_faq.php' : 'faq.php');
   echo
     "<script>
