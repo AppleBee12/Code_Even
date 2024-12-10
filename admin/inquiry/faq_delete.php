a8ae24ebd6862a687503c9779eeafb7c2c60ef5f
YUNA9627 <yuna2175@naver.com>
2024-12-11 03:53:33 +0900

    📝 docs: 이미지 저장 sql 작성중

--- admin/inquiry/faq_delete.php
+++ admin/inquiry/faq_delete.php
@@ -7,46 +7,51 @@
   $target_sql = "SELECT faq.target FROM faq WHERE fqid = $fqid";
   $target_result = $mysqli->query($target_sql);
 
-  $image_sql = "SELECT file_name FROM summer_images WHERE table_id = $fqid";
-  $image_result  = $mysqli->query($image_sql);
-
   if ($target_result) {
     $data = $target_result->fetch_object();
     $target = $data->target;
 
     // print_r($data); 
-  
-    $sql = "DELETE FROM faq WHERE fqid = $fqid";
-    $result = $mysqli->query($sql);
+
+    $image_sql = "SELECT file_name FROM summer_images WHERE table_id = $fqid";
+    $image_result  = $mysqli->query($image_sql);
 
     if ($image_result->num_rows > 0) {
 
       $row = $image_result->fetch_assoc();
       $imageFileName = $row['file_name'];
-
+      $baseUrl = 'http://' . $_SERVER['HTTP_HOST'] . '/code_even/admin/upload/summernote/';
+      $imageFileNameWithoutUrl = str_replace($baseUrl, '', $imageFileName);
+      
       // 실제 파일 시스템에서 이미지 삭제
-      $imagePath = $_SERVER['DOCUMENT_ROOT'] . '/code_even/admin/upload/summernote/' . $imageFileName;
+      $imagePath = $_SERVER['DOCUMENT_ROOT'] . '/code_even/admin/upload/summernote/' . $imageFileNameWithoutUrl;
   
       if (file_exists($imagePath)) {
         unlink($imagePath); // 파일 삭제
-        echo "파일 '$imageFileName'이 성공적으로 삭제되었습니다.";
 
+        // 데이터베이스 이미지 삭제
         $delete_sql = "DELETE FROM summer_images WHERE table_id = $fqid";
         $delete_result = $mysqli->query($delete_sql);
 
         if ($delete_result) {
-          echo "데이터베이스에서 레코드가 삭제되었습니다.<br>";
-      } else {
-          echo "데이터베이스에서 레코드를 삭제하는 데 실패했습니다.<br>";
-      }
+          echo "<script>console.log('데이터베이스에서 레코드가 삭제되었습니다.');</script>";
+        } else {
+          echo "<script>console.log('데이터베이스에서 레코드를 삭제하는 데 실패했습니다.');</script>";
+        }
+
         
       } else {
-        echo "파일 '$imageFileName'이 존재하지 않습니다.";
+        echo "<script>console.log('파일 \"$imageFileNameWithoutUrl\"이 존재하지 않습니다.');</script>";
       }
 
+
     } else {
-        echo "데이터베이스에서 이미지를 삭제하는 데 실패했습니다.";
+      echo "<script>console.log('해당 fqid에 해당하는 이미지를 찾을 수 없습니다.');</script>";
     }
+
+    // FAQ 데이터 삭제
+    $sql = "DELETE FROM faq WHERE fqid = $fqid";
+    $result = $mysqli->query($sql);
 
     if ($result) {
       $redirect_url = ($target === 'teacher') ? 'teacher_faq.php' : (($target === 'student') ? 'student_faq.php' : 'index.php');
@@ -54,7 +59,7 @@
         "<script>
           confirm('글을 삭제하시겠습니까?');
           alert('삭제가 완료되었습니다.');
-          
+          location.href = '$redirect_url';
         </script>";
     } else {
       echo
@@ -63,6 +68,8 @@
           history.back();
         </script>";
     }
+
+
   } else {
     echo
       "<script>
