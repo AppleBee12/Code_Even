b94e78d10501227f1aa019d7155dda982eafa92b
YUNA9627 <yuna2175@naver.com>
2024-12-10 18:14:39 +0900

    📝 docs: 이미지컬럼 세션 값 추가

--- admin/inc/upload_image.php
+++ admin/inc/upload_image.php
@@ -1,23 +1,28 @@
 <?php
+session_start();
+include_once($_SERVER['DOCUMENT_ROOT'] . '/CODE_EVEN/admin/inc/dbcon.php');
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $uploadDir = $_SERVER['DOCUMENT_ROOT'] . '/code_even/admin/upload/summernote/'; // 이미지 저장 경로
-   
-    $fileName = uniqid() . '_' . basename($_FILES['file']['name']);
-    $uploadFilePath = $uploadDir . $fileName;
+  $uploadDir = $_SERVER['DOCUMENT_ROOT'] . '/code_even/admin/upload/summernote/'; // 이미지 저장 경로
+  
+  $fileName = uniqid() . '_' . basename($_FILES['file']['name']);
+  $uploadFilePath = $uploadDir . $fileName;
 
-    // 브라우저에서 접근 가능한 경로
-    $accessibleUrlBase = 'http://' . $_SERVER['HTTP_HOST'] . '/code_even/admin/upload/summernote/';
+  // 브라우저에서 접근 가능한 경로
+  $accessibleUrlBase = 'http://' . $_SERVER['HTTP_HOST'] . '/code_even/admin/upload/summernote/';
 
-    if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadFilePath)) {
-        echo json_encode([
-            'status' => 'success',
-            'imageUrl' => $accessibleUrlBase . $fileName, // 브라우저에서 접근 가능한 경로
-        ]);
-    } else {
-        echo json_encode([
-            'status' => 'error',
-            'message' => '이미지 업로드 실패',
-        ]);
-    }
+  if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadFilePath)) {
+    echo json_encode([
+        'status' => 'success',
+        'imageUrl' => $accessibleUrlBase . $fileName, // 브라우저에서 접근 가능한 경로
+    ]);
+    $_SESSION['imageUrl'] = $accessibleUrlBase . $fileName;
+    
+  } else {
+    echo json_encode([
+        'status' => 'error',
+        'message' => '이미지 업로드 실패',
+    ]);
+  }
 }
 ?>
