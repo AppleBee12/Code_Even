c749d76654bb0eddc3086c0320218ec2b665eac5
eunjin6-6 <dmswls616@gmail.com>
2024-12-17 00:22:25 +0900

    📝 docs: 장바구니 제목 스타일 변경

--- front/cart.php
+++ front/cart.php
@@ -4,7 +4,14 @@
 
 <div class="white container_wrap">
   <div class="container">
-    <h2 class="headt4">장바구니</h2>
+    <h2 class="headt5">장바구니</h2>
+    <div class="row">
+      <div class="cart_detail col-8">
+      전체 선택 3/3
+      </div>
+      <div class="cart_total col-4"></div>
+
+    </div>
   </div>
 </div>
 
