8e245551b2789af917637f1fe02da6f964c7cacc
eunjin6-6 <dmswls616@gmail.com>
2024-12-16 14:49:15 +0900

    🚚 rename: 결제내역 페이지 이름 변경

---
+++ front/mypage/mypage_paid.php
@@ -0,0 +1,22 @@
+<?php
+$title = '마이페이지-결제내역';
+include_once($_SERVER['DOCUMENT_ROOT'] . '/CODE_EVEN/front/inc/mypage_header.php');
+
+?>
+<!--제목 시작-->
+<div class="mypage_title_wrapper">
+  <p class="mypage_title headt5">결제내역</p>
+</div>
+<!--제목 끝-->
+<div class="">
+
+
+
+</div>
+</div><!--여기부터는 마이페이지 헤더의 닫는 태그-->
+</section>
+</div>
+</div>
+</div>
+<?php include_once($_SERVER['DOCUMENT_ROOT'] . '/CODE_EVEN/front/inc/footer.php');
+?>
