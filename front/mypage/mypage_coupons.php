dc943c39c6997c26ad75a328a0ef7030e686a195
chaerimjo-web <chaerim010710@gmail.com>
2024-12-16 13:03:28 +0900

    📝 docs: css수정

--- front/mypage/mypage_coupons.php
+++ front/mypage/mypage_coupons.php
@@ -66,7 +66,7 @@ while ($data = $result->fetch_object()) {
   }
 
   .card {
-    width: 45%;
+    width: 46%;
     height: 210px;
     .c-img img{
       height: 210px;
@@ -76,6 +76,9 @@ while ($data = $result->fetch_object()) {
   .cps {
     /* gap: 110px; */
     height: 250px;
+  }
+  .text-bd-secondary{
+    color: var(--bk700);
   }
   
 </style>
