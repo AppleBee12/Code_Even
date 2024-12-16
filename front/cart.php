a314c5327c986c0bca839681f0d811c72ce7d059
eunjin6-6 <dmswls616@gmail.com>
2024-12-16 14:48:22 +0900

    📝 docs: cart.php 생성

---
+++ front/cart.php
@@ -0,0 +1,14 @@
+<?php
+  include_once($_SERVER['DOCUMENT_ROOT'] . '/code_even/front/inc/header.php');
+?>
+
+<div class="white container_wrap">
+  <div class="container">
+    <h2 class="headt4">장바구니</h2>
+  </div>
+</div>
+
+
+<?php
+  include_once($_SERVER['DOCUMENT_ROOT'] . '/code_even/front/inc/footer.php');
+?>
