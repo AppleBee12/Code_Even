251fb36017e853dcfdd25e98c7e5f62e4eaa2bad
AppleBee12 <mieldeluna@naver.com>
2024-12-16 18:22:27 +0900

    📝 Docs: 레시피강좌 html추가

--- front/what_recipe/what_recipe.php
+++ front/what_recipe/what_recipe.php
@@ -18,27 +18,59 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/CODE_EVEN/front/inc/header.php');
   </section>
   <section class="sec02">
     <div class="container">
-
+      <img src="../images/what_recipe_sec02student.png" alt="">
     </div>
   </section>
 
   <section class="sec03 white">
     <div class="container">
       <div class="title_wrapper">
-        <p class="headt4"></p>
-        <p class="headt3"></p>
+        <p class="headt4">바로 바로 생각이 안난다구요?</p>
+        <p class="headt3">한번 결제한 강의는 평생 무제한 수강!</p>
+      </div>
+      <div>
+        <img src="../images/what_recipe_sec03cook.png" alt="">
+      </div>
+    </div>
+  </section>
+  <section class="sec04">
+    <div class="container">
+      <div>
+        <p>recipe 01</p>
+        <div>
+          <p>이해에 필요한 필수 개념부터</p>
+          <p>필요한 언어까지</p>
+        </div>
+        <img src="../images/what_recipe_sec04_icon01.png" alt="">
+      </div>
+      <div>
+        <p>recipe 02</p>
+        <div>
+          <p>강의가 끝날 때마다</p>
+          <p>핵심내용이 담긴 퀴즈</p>
+        </div>
+        <img src="../images/what_recipe_sec04_icon02.png" alt="">
+      </div>
+      <div>
+        <p>recipe 03</p>
+        <div>
+          <p>원하는 때에 언제든</p>
+          <p>다시 찾아볼 수 있도록</p>
+        </div>
+        <img src="../images/what_recipe_sec04_icon03.png" alt="">
       </div>
 
     </div>
   </section>
-  <section class="sec04">
-    <div class="container"></div>
-  </section>
   <section class="sec05 white">
     <div class="container">
       <div class="title_wrapper">
-        <p class="headt4"></p>
-        <p class="headt3"></p>
+        <p class="headt4">한시가 급한 프로젝트, 찾을 시간도 아깝다면?</p>
+        <p class="headt3">코드이븐의 RECIPE 레시피 강좌 !</p>
+      </div>
+      <div>
+        <p>이런 분들을 위한 강의에요!</p>
+        <div></div>
       </div>
     </div>
   </section>
