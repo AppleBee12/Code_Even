25f45c5272d81411e07185223a5b6d25eeff3dc4
AppleBee12 <mieldeluna@naver.com>
2024-12-16 12:14:34 +0900

    📝 Docs: 블로그php생성

---
+++ front/community/blog.php
@@ -0,0 +1,14 @@
+<?php
+$title = '블로그';
+include_once($_SERVER['DOCUMENT_ROOT'] . '/CODE_EVEN/front/inc/header.php');
+?>
+<div class="container">
+  <section>
+    
+  </section>
+
+</div>
+
+<?php include_once($_SERVER['DOCUMENT_ROOT'] . '/CODE_EVEN/front/inc/footer.php');
+?>
+
