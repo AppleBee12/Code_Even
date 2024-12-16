04dc54a3fc9272ed577ed2430b3baa4bbd7f4423
AppleBee12 <mieldeluna@naver.com>
2024-12-16 13:08:23 +0900

    📝 Docs: what_recipe 그림추가

--- front/what_recipe/what_recipe.php
+++ front/what_recipe/what_recipe.php
@@ -3,34 +3,60 @@ $title = '레시피 강좌란?';
 include_once($_SERVER['DOCUMENT_ROOT'] . '/CODE_EVEN/front/inc/header.php');
 ?>
 <div>
-  <section class="white">
+  <section class="sec01 white">
     <div class="container">
-      <div>
-        <p>오직 코드이븐에서만</p>
-        <p>현업자 사이 입소문난 그 코스!</p>
+      <div class>
+        <p class="headt3">오직 코드이븐에서만</p>
+        <p class="headt3 b">현업자 사이 입소문난 그 코스!</p>
       </div>
       <div class="headt1">레시피</div>
-      <p>요리의 레시피처럼</p>
-      <p>알고있어야 할 개념과 내용만 쏙쏙 알려드려요</p>
+      <p class="headt3">요리의 레시피처럼</p>
+      <p class="headt3">알고있어야 할 개념과 내용만 쏙쏙 알려드려요</p>
+      <img src="../images/what_recipe_sec01recipe.png" alt="">
+    </div>
+    <img src="../images/what_recipe_sec01cheif.png" alt="">
+  </section>
+  <section class="sec02">
+    <div class="container">
+
     </div>
   </section>
-  <section class="">
+
+  <section class="sec03 white">
+    <div class="container">
+      <div class="title_wrapper">
+        <p class="headt4"></p>
+        <p class="headt3"></p>
+      </div>
+
+    </div>
+  </section>
+  <section class="sec04">
     <div class="container"></div>
   </section>
-  <section class="white">
-    <div class="container"></div>
+  <section class="sec05 white">
+    <div class="container">
+      <div class="title_wrapper">
+        <p class="headt4"></p>
+        <p class="headt3"></p>
+      </div>
+    </div>
   </section>
-  <section class="">
-    <div class="container"></div>
+  <section class="sec06">
+    <div class="container">
+      <div class="title_wrapper">
+        <p class="headt4"></p>
+        <p class="headt3"></p>
+      </div>
+    </div>
   </section>
-  <section class="white">
-    <div class="container"></div>
-  </section>
-  <section class="">
-    <div class="container"></div>
-  </section>
-  <section class="white">
-    <div class="container"></div>
+  <section class="sec07">
+    <div class="container">
+      <div class="title_wrapper">
+        <p class="headt4"></p>
+        <p class="headt3"></p>
+      </div>
+    </div>
   </section>
 </div>
 
