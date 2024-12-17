046442c10311e8d7f78224ec94838a14871a8c26
chaerimjo-web <chaerim010710@gmail.com>
2024-12-17 17:34:12 +0900

    📝 docs: 경로 변경

--- front/inc/header.php
+++ front/inc/header.php
@@ -134,7 +134,7 @@ if (isset($_GET['code'])) {
     case 'mypage_header.php':
     case 'mypage_lecture.php':
     case 'mypage_coupons.php':
-    case 'mypage_info.php':
+    case 'mypage_info_edit.php':
     case 'mypage_paid.php':
     case 'mypage_copy.php':
       echo '<link rel="stylesheet" href="http://' . $_SERVER['HTTP_HOST'] . '/code_even/front/css/mypage_header.css">';
@@ -168,7 +168,7 @@ if (isset($_GET['code'])) {
       break;
   }
   switch ($page) { //mypage_coupons.css
-    case 'mypage_info.php':
+    case 'mypage_info_edit.php':
       echo '<link rel="stylesheet" href="http://' . $_SERVER['HTTP_HOST'] . '/code_even/front/css/mypage_info.css">';
       break;
   }
@@ -392,7 +392,7 @@ if (isset($_GET['code'])) {
                     <li class="list_pt"><a href="http://<?= $_SERVER['HTTP_HOST']; ?>/code_even/front/cart.php"><i
                           class="bi bi-cart"></i><span>장바구니</span></a></li>
                     <li class="list_pb"><a href=""><i class="bi bi-heart"></i><span>찜한 강좌</span></a></li>
-                    <li class="list_pt"><a href="http://<?= $_SERVER['HTTP_HOST']; ?>/code_even/front/mypage/mypage_info.php"><i class="bi bi-person-circle"></i><span>기본 정보 설정</span></a></li>
+                    <li class="list_pt"><a href="http://<?= $_SERVER['HTTP_HOST']; ?>/code_even/front/mypage/mypage_info_edit.php"><i class="bi bi-person-circle"></i><span>기본 정보 설정</span></a></li>
                     <li class="list_pb"><a href="http://<?= $_SERVER['HTTP_HOST']; ?>/code_even/front/login/logout.php"><i
                           class="bi bi-box-arrow-right"></i><span>로그아웃</span></a></li>
                   </ul>
