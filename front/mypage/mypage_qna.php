71ff33bc0cb1ab964c19a272aa8c2700b8ac52b2
YUNA9627 <yuna2175@naver.com>
2024-12-18 17:30:47 +0900

    📝 docs: 마이페이지 1:1문의 페이지 작성

--- front/mypage/mypage_qna.php
+++ front/mypage/mypage_qna.php
@@ -2,6 +2,32 @@
 $title = '마이페이지-문의글보기';
 include_once($_SERVER['DOCUMENT_ROOT'] . '/CODE_EVEN/front/inc/mypage_header.php');
 
+// 강의 문의
+
+
+
+// 서비스문의
+$question_sql = "SELECT admin_question.*, user.uid, admin_answer.aaid 
+                FROM admin_question 
+                JOIN user ON admin_question.uid = user.uid 
+                LEFT JOIN admin_answer ON admin_question.aqid = admin_answer.aqid 
+                WHERE user.userid = '" . (isset($_SESSION['AUID']) ? $_SESSION['AUID'] : '') . "'";
+$question_result = $mysqli->query($question_sql);
+
+$categoryNames = [
+  1 => '결제/환불',
+  2 => '강의',
+  3 => '쿠폰',
+  4 => '가입/탈퇴',
+  5 => '기타',
+  6 => '수료',
+  7 => '정산',
+  8 => '강사',
+];
+
+while ($qdata = $question_result->fetch_object()) {
+  $qdataArr[] = $qdata;
+}
 ?>
 <!--탭 메뉴 시작-->
 <nav>
@@ -17,23 +43,34 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/CODE_EVEN/front/inc/mypage_header.php
     <div class="mypage_title_wrapper">
       <p class="mypage_title headt5">나의 문의 내역</p>
     </div>
+
     <div>
-    <table class="table list_table">
-    <thead>
-      <tr>
-        <th scope="col">
-          <input class="form-check-input" type="checkbox" id="allCheck">
-        </th>
-        <th scope="col">번호</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th></th>
-        <td></td>
-      </tr>
-    </tbody>
-  </table>
+      <form class="table qna_form">
+        <div class="d-flex justify-content-between align-items-center">
+          <p>총 건</p>
+          <button type="submit">1:1 문의하기</button>
+        </div>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">등록일</th>
+              <th scope="col">강좌명</th>
+              <th scope="col">제목</th>
+              <th scope="col">상태</th>
+              <th scope="col">삭제</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th></th>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </form>
     </div>  
 
     <!--제목 끝-->
@@ -44,8 +81,44 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/CODE_EVEN/front/inc/mypage_header.php
       <p class="mypage_title headt5">나의 문의 내역</p>
     </div>
     <!--제목 끝-->  
-    <div>
+    <div class="qna_content">
+      <form action="http://<?= $_SERVER['HTTP_HOST']; ?>/code_even/front/mypage/mypage_qna_question.php" class="qna_form">
+        <div class="d-flex justify-content-between align-items-center">
+          <p>총 <?= count($qdataArr); ?>건</p>
+          <button type="submit">1:1 문의하기</button>
+        </div>
+        <table class="table qna_table">
+          <thead>
+            <tr>
+              <th scope="col">등록일</th>
+              <th scope="col">문의 분류</th>
+              <th scope="col">제목</th>
+              <th scope="col">상태</th>
+              <th scope="col">삭제</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+              foreach($qdataArr as $question) {
+                $categoryName = $categoryNames[$question->category] ?? '알 수 없음';
+            ?>
+            <tr>
+              <th><?=$question->regdate;?></th>
+              <td><?=$categoryName;?></td>
+              <td>
+                <a href="http://<?= $_SERVER['HTTP_HOST']; ?>/code_even/front/mypage/mypage_qna_question_details.php?aqid=<?=$question->aqid;?>" class="underline"><?=$question->qtitle;?></a>
+              </td>
+              <td><?= $question->aqid ? "답변완료" : "답변대기"; ?></td>
+              <td><a href="http://<?= $_SERVER['HTTP_HOST']; ?>/code_even/front/service/qna_delete.php?aqid=<?= $question->aqid; ?>">X</a></td>
+            </tr>
+            <?php
+              }
+            ?>
+          </tbody>
+        </table>
+      </form>
     </div>
+
   </div><!-- 탭메뉴2 끝-->
 </div>
 
