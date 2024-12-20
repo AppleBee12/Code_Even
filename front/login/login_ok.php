2e44e4eed68917f67e1b078bcfbdf4947247bad1
dmswls616 <61467603+dmswls616@users.noreply.github.com>
2024-12-20 09:26:13 +0900

    📝 docs: 로그인 시 cart 테이블에 로그인정보 추가

--- front/login/login_ok.php
+++ front/login/login_ok.php
@@ -9,13 +9,27 @@ $password = hash('sha512', $userpw);
 $sql = "SELECT * FROM user WHERE userid = '$userid' and userpw = '$password'";
 $result = $mysqli->query($sql);
 $data = $result->fetch_object();
+
 if($data){
-  $update_sql = "UPDATE user SET last_date = now() WHERE uid = $data->uid";
-  $update_result = $mysqli->query($update_sql);
   $_SESSION['AUID'] = $data->userid;
   $_SESSION['AUNAME'] = $data->username;
   $_SESSION['AULEVEL'] = $data->user_level;
   $_SESSION['UID'] = $data->uid;
+
+  // 마지막 로그인 시간 업데이트
+  $update_sql = "UPDATE user SET last_date = now() WHERE uid = $data->uid";
+  $update_result = $mysqli->query($update_sql);
+
+  // 세션 아이디를 기반으로 cart 테이블 데이터 업데이트
+  $session_id = session_id();
+  $cart_update_sql = 
+  "UPDATE cart 
+      SET uid = {$data->uid}, userid = '{$data->userid}' 
+      WHERE ssid = '$session_id'
+  ";
+  $mysqli->query($cart_update_sql);
+
+  
 
   if ($_SESSION['AULEVEL'] == 100) {
     echo "<script>
