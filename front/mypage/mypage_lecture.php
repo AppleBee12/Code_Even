1d411f2901e44739134101c4a50d642ac4456389
eunhwa-a11y <ttab136@gmail.com>
2024-12-23 04:23:41 +0900

    📝 docs: 연결 페이지 수정정

--- front/mypage/mypage_lecture.php
+++ front/mypage/mypage_lecture.php
@@ -63,13 +63,18 @@ foreach ($classArr as $class) {
 
       <?php
       foreach ($groupedClasses as $lectureId => $classes):
-      ?>
+        ?>
         <div class="my_lecture mb-4">
           <div class="my_lec_top d-flex">
-            <img src="<?= isset($classes[0]->lecture_image) ? htmlspecialchars($classes[0]->lecture_image) : 'default.jpg'; ?>" alt="강좌 이미지">
+            <img
+              src="<?= isset($classes[0]->lecture_image) ? htmlspecialchars($classes[0]->lecture_image) : 'default.jpg'; ?>"
+              alt="강좌 이미지">
             <div class="d-flex flex-column justify-content-evenly">
-              <p class="headt5"><?= isset($classes[0]->lecture_title) ? htmlspecialchars($classes[0]->lecture_title) : '강좌 제목 없음'; ?></p>
-              <p><b><?= isset($classes[0]->lecture_teacher) ? htmlspecialchars($classes[0]->lecture_teacher) : '강사명 없음'; ?></b> | <span>레시피강좌</span></p>
+              <p class="headt5">
+                <?= isset($classes[0]->lecture_title) ? htmlspecialchars($classes[0]->lecture_title) : '강좌 제목 없음'; ?></p>
+              <p>
+                <b><?= isset($classes[0]->lecture_teacher) ? htmlspecialchars($classes[0]->lecture_teacher) : '강사명 없음'; ?></b>
+                | <span>레시피강좌</span></p>
             </div>
           </div>
           <!-- 강좌 디테일 시작 -->
@@ -84,7 +89,7 @@ foreach ($classArr as $class) {
                   <?php
                   $lectureStartDate = isset($classes[0]->regdate) ? $classes[0]->regdate : null; // 강의 시작일
                   $lecturePeriod = isset($classes[0]->lecture_period) ? $classes[0]->lecture_period : 0; // 수료 기간
-
+                
                   if ($lectureStartDate) {
                     $startDate = new DateTime($lectureStartDate); // 시작일 생성
                     $endDate = $startDate->modify("+$lecturePeriod days"); // 수료 기간 추가
@@ -106,7 +111,7 @@ foreach ($classArr as $class) {
                     <?php
                     $totalLectures = count($classes); // 세부 강의 전체 개수
                     $completedLectures = 0; // 수강 완료된 강의 개수 초기화
-
+                  
                     foreach ($classes as $class) {
                       if (isset($class->quiz_score) && $class->quiz_score > 0) {
                         $completedLectures++;
@@ -116,8 +121,10 @@ foreach ($classArr as $class) {
                     $completionRate = $totalLectures > 0 ? round(($completedLectures / $totalLectures) * 100) : 0;
                     //echo $completionRate;
                     ?>
-                    <div class="progress" role="progressbar" aria-valuenow="<?= $completionRate; ?>" aria-valuemin="0" aria-valuemax="100">
-                      <div class="progress-bar progress-bar-striped bg-danger progress-bar-animated" style="width:  <?= $completionRate; ?>%">
+                    <div class="progress" role="progressbar" aria-valuenow="<?= $completionRate; ?>" aria-valuemin="0"
+                      aria-valuemax="100">
+                      <div class="progress-bar progress-bar-striped bg-danger progress-bar-animated"
+                        style="width:  <?= $completionRate; ?>%">
                         <?= $completionRate; ?>%
                       </div>
                     </div>
@@ -128,11 +135,13 @@ foreach ($classArr as $class) {
                   </li>
                 </ul>
                 <div class="my_lec_btn d-flex mt-3 gap-2">
-                  <button type="button" class="btn btn-outline-dark btn-sm" data-bs-toggle="modal" data-bs-target="#howToGetCertificate">
+                  <button type="button" class="btn btn-outline-dark btn-sm" data-bs-toggle="modal"
+                    data-bs-target="#howToGetCertificate">
                     수료기준
                   </button>
                   <!-- Modal -->
-                  <div class="modal fade" id="howToGetCertificate" tabindex="-1" aria-labelledby="howToGetCertificateLabel" aria-hidden="true">
+                  <div class="modal fade" id="howToGetCertificate" tabindex="-1"
+                    aria-labelledby="howToGetCertificateLabel" aria-hidden="true">
                     <div class="modal-dialog modal-dialog-centered">
                       <div class="modal-content">
                         <div class="modal-header">
@@ -152,14 +161,20 @@ foreach ($classArr as $class) {
               <!-- 그래프 영역 -->
               <div class="my_lec_graph_wrapper d-flex">
                 <div class="my_lec_graph d-flex flex-column align-items-center">
-                  <div class="donut-chart" style="--percentage: <?= $completionRate = $totalLectures > 0 ? round(($completedLectures / $totalLectures) * 100) : 0; ?>%;">
-                    <div class="percentage-label"><?= $completionRate = $totalLectures > 0 ? round(($completedLectures / $totalLectures) * 100) : 0; ?>%</div>
+                  <div class="donut-chart"
+                    style="--percentage: <?= $completionRate = $totalLectures > 0 ? round(($completedLectures / $totalLectures) * 100) : 0; ?>%;">
+                    <div class="percentage-label">
+                      <?= $completionRate = $totalLectures > 0 ? round(($completedLectures / $totalLectures) * 100) : 0; ?>%
+                    </div>
                   </div>
                   <p>강의</p>
                 </div>
                 <div class="my_lec_graph d-flex flex-column align-items-center">
-                  <div class="donut-chart" style="--percentage: <?= $completionRate = $totalLectures > 0 ? round(($completedLectures / $totalLectures) * 100) : 0; ?>%;">
-                    <div class="percentage-label"><?= $completionRate = $totalLectures > 0 ? round(($completedLectures / $totalLectures) * 100) : 0; ?>%</div>
+                  <div class="donut-chart"
+                    style="--percentage: <?= $completionRate = $totalLectures > 0 ? round(($completedLectures / $totalLectures) * 100) : 0; ?>%;">
+                    <div class="percentage-label">
+                      <?= $completionRate = $totalLectures > 0 ? round(($completedLectures / $totalLectures) * 100) : 0; ?>%
+                    </div>
                   </div>
                   <p>퀴즈</p>
                 </div>
@@ -167,7 +182,7 @@ foreach ($classArr as $class) {
                   <?php
                   $totalTests = 0; // 전체 시험 개수
                   $completedTests = 0; // 완료된 시험 개수
-
+                
                   foreach ($classes as $class) {
                     if (isset($class->test_id)) {
                       $totalTests++; // 시험이 있으면 전체 시험 수 증가
@@ -218,14 +233,15 @@ foreach ($classArr as $class) {
                     </div>
                   </div>
                   <div class="goto_lec_btn">
-                    <button href="http://<?= $_SERVER['HTTP_HOST']; ?>/code_even/front/lecture_detail.php?detail_id=<?= isset($class->detail_id) ? $class->detail_id : 0; ?>" class="btn btn-secondary">강의보러가기</button>
+                    <a href="http://<?= $_SERVER['HTTP_HOST']; ?>/code_even/front/lecture_detail.php?detail_id=<?= isset($class->detail_id) ? $class->detail_id : 0; ?>"
+                      class="btn btn-secondary">강의보러가기</a>
                   </div>
                 </div><!--한 강의 내용 끝-->
               <?php endforeach; ?>
             </div><!-- 세부 강의 1강2강3강 목록 끝 -->
           </div><!-- 강좌 디테일 끝 -->
         </div><!-- 강의목록 끝 -->
-      <?php
+        <?php
       endforeach;
       ?>
     </div>
@@ -287,7 +303,7 @@ foreach ($classArr as $class) {
     document.body.appendChild(iframe);
 
     // PDF 파일이 로드된 후 인쇄
-    iframe.onload = function() {
+    iframe.onload = function () {
       iframe.contentWindow.print(); // iframe 내에서 print() 호출
     };
   }
