0e295af056c4b4cdb882e8b4cb4b9d8b007c715e
YUNA9627 <yuna2175@naver.com>
2024-12-23 07:09:56 +0900

    📝 docs: 작성 파일 추가

--- front/mypage/mypage_review_write.php
+++ front/mypage/mypage_review_write.php
@@ -2,12 +2,15 @@
 $title = '마이페이지-1:1문의하기';
 include_once($_SERVER['DOCUMENT_ROOT'] . '/CODE_EVEN/front/inc/mypage_header.php');
 
-$question_sql = "SELECT admin_question.*, user.* 
-                FROM admin_question 
-                JOIN user ON admin_question.uid = user.uid";
-$question_result = $mysqli->query($question_sql);
-
-$qdata = $question_result->fetch_object();
+$sql = "SELECT class_data.*, lecture.* 
+        FROM class_data 
+        JOIN lecture ON class_data.leid = lecture.leid 
+        WHERE class_data.uid = '" . (isset($_SESSION['UID']) ? $_SESSION['UID'] : '') . "'";
+$result = $mysqli->query($sql);
+$dataArr = [];
+while ($data = $result->fetch_object()) {
+    $dataArr[] = $data;
+}
 ?>
 <div class="tab-content" id="nav-tabContent"><!--탭 메뉴 내용 시작-->
   <div class="tab-pane fade show active" id="nav-myLecTab1" role="tabpanel" aria-labelledby="nav-myLecTab1-tab"><!-- 탭메뉴1 -->
@@ -37,42 +40,50 @@ $qdata = $question_result->fetch_object();
               <tr class="none">
                 <th scope="row">강좌명</th>
                 <td>
-                  <select class="form-select w-50" name="category" required>
-                    <option value="">분류 선택</option>
+                  <select class="form-select w-50" name="cdid" required>
+                  <?php 
+                    foreach($dataArr as $data){
+                  ?>
+                    <option value="<?= htmlspecialchars($data->cdid) ?>">
+                    [<?= htmlspecialchars($data->name) ?>] <?= htmlspecialchars($data->title) ?>
+                    </option>
+                  <?php
+                    }
+                  ?>
                   </select>
                 </td>
               </tr>
               <tr class="none">
                 <th scope="row">평가</th>
                 <td>
-                <div class="rating">
-                  <label class="rating__label rating__label--half" for="star1">
-                    <input type="radio" id="star1" class="rating__input" name="rating" value="">
-                    <span class="star-icon"></span>
-                  </label>
-                  <label class="rating__label rating__label--full" for="star2">
-                    <input type="radio" id="star2" class="rating__input" name="rating" value="">
-                    <span class="star-icon"></span>
-                  </label>
-                  <label class="rating__label rating__label--full" for="star3">
-                    <input type="radio" id="star3" class="rating__input" name="rating" value="">
-                    <span class="star-icon"></span>
-                  </label>
-                  <label class="rating__label rating__label--full" for="star4">
-                    <input type="radio" id="star4" class="rating__input" name="rating" value="">
-                    <span class="star-icon"></span>
-                  </label>
-                  <label class="rating__label rating__label--full" for="star5">
-                    <input type="radio" id="star5" class="rating__input" name="rating" value="">
-                    <span class="star-icon"></span>
-                  </label>
-                </div>
+                  <div class="rating">
+                    <label class="rating__label rating__label--half" for="star1">
+                      <input type="radio" id="star1" class="rating__input" name="rating" value="">
+                      <span class="star-icon"></span>
+                    </label>
+                    <label class="rating__label rating__label--full" for="star2">
+                      <input type="radio" id="star2" class="rating__input" name="rating" value="">
+                      <span class="star-icon"></span>
+                    </label>
+                    <label class="rating__label rating__label--full" for="star3">
+                      <input type="radio" id="star3" class="rating__input" name="rating" value="">
+                      <span class="star-icon"></span>
+                    </label>
+                    <label class="rating__label rating__label--full" for="star4">
+                      <input type="radio" id="star4" class="rating__input" name="rating" value="">
+                      <span class="star-icon"></span>
+                    </label>
+                    <label class="rating__label rating__label--full" for="star5">
+                      <input type="radio" id="star5" class="rating__input" name="rating" value="">
+                      <span class="star-icon"></span>
+                    </label>
+                  </div>
                 </td>
               </tr>
               <tr class="none">
                 <th scope="row">제목</th>
                 <td>
-                  <input type="text" name="title" class="form-control w-75" id="title" placeholder="제목을 입력해주세요." required>
+                  <input type="text" name="rtitle" class="form-control w-75" id="title" placeholder="제목을 입력해주세요." required>
                 </td>
               </tr>
               <tr class="none">
