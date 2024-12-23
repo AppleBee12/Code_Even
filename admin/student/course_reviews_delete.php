5d0e7a01aedf8f22856793daaa05538f10eb4f60
YUNA9627 <yuna2175@naver.com>
2024-12-23 10:22:55 +0900

    📝 docs: confirm 취소 기능 추가 오류로 인한 삭제 기능 다시 수정

--- admin/student/course_reviews_delete.php
+++ admin/student/course_reviews_delete.php
@@ -2,35 +2,33 @@
 $title = "수강 후기";
 include_once($_SERVER['DOCUMENT_ROOT'] . '/CODE_EVEN/admin/inc/dbcon.php');
 
-  // JavaScript를 사용해 확인창을 표시
-  if (!isset($_GET['delete']) || $_GET['delete'] !== 'true') {
-    echo "
-    <script>
-        if (confirm('글을 삭제하시겠습니까?')) {
-            location.href = 'course_reviews.php?rvid=" . $_GET['rvid'] . "&delete=true';
-        } else {
-            alert('삭제가 취소되었습니다.');
-            location.href = 'course_reviews.php';
-        }
-    </script>";
-    exit; // 아래 PHP 코드 실행 방지
-  }
+if (isset($_GET['rvid']) && isset($_GET['confirm']) && $_GET['confirm'] == "true") {
+  $rvid = $_GET['rvid'];
+  $sql = "DELETE FROM review WHERE rvid = $rvid";
+  $result = $mysqli->query($sql);
 
-$rvid = $_GET['rvid'];
-$sql = "DELETE FROM review WHERE rvid = $rvid";
-$result = $mysqli->query($sql);
-
-if ($result) {
-  echo
+  if ($result) {
+  echo 
   "<script>
-    alert('삭제가 완료되었습니다.');
-    location.href='course_reviews.php';
+      alert('삭제가 완료되었습니다.');
+      location.href = 'course_reviews.php?delete=true';
   </script>";
-} else {
-  echo
+  } else {
+    echo 
     "<script>
-      alert('글 삭제 실패');
-      history.back();
+        alert('글 삭제 실패');
+        history.back();
     </script>";
+  }
+} else {
+  echo 
+  "<script>
+      if (confirm('글을 삭제하시겠습니까?')) {
+          location.href = '?rvid=" . $_GET['rvid'] . "&confirm=true';
+      } else {
+          alert('삭제가 취소되었습니다.');
+          location.href = 'course_reviews.php';
+      }
+  </script>";
 }
 ?>
