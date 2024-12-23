1972f3187bf339cf5ed1ea3696de493297620651
YUNA9627 <yuna2175@naver.com>
2024-12-23 11:05:44 +0900

    📝 docs: confirm 취소 기능 추가 오류로 인한 삭제 기능 다시 수정

--- admin/inquiry/admin_qna_answer_ok.php
+++ admin/inquiry/admin_qna_answer_ok.php
@@ -1,34 +1,41 @@
 <?php
 include_once($_SERVER['DOCUMENT_ROOT'] . '/CODE_EVEN/admin/inc/dbcon.php');
 
-// print_r($_POST);
 $aqid = $_POST['aqid'];
 $acontent = $_POST['acontent'];
 
-$admin_qna_sql = "
-    INSERT INTO admin_answer (aqid , acontent)
-    SELECT aqid, '$acontent' 
-    FROM admin_question 
-    WHERE aqid = $aqid
+if (isset($_GET['confirm']) && $_GET['confirm'] == "true") {
+    $admin_qna_sql = "
+        INSERT INTO admin_answer (aqid, acontent)
+        SELECT aqid, '$acontent'
+        FROM admin_question
+        WHERE aqid = $aqid
     ";
 
-// print_r($admin_qna_sql);
+    $admin_qna_result = $mysqli->query($admin_qna_sql);
 
-$admin_qna_result = $mysqli->query($admin_qna_sql);
-
-if ($admin_qna_result === true) {
-  echo
-    "<script>
-    confirm('글을 등록하시겠습니까?');
-    alert('등록이 완료되었습니다.');
-    location.href = 'admin_qna.php';
-  </script>";
+    if ($admin_qna_result === true) {
+        echo 
+        "<script>
+            alert('등록이 완료되었습니다.');
+            location.href = 'admin_qna.php';
+        </script>";
+    } else {
+        echo 
+        "<script>
+            alert('글쓰기 실패');
+            history.back();
+        </script>";
+    }
 } else {
-  echo
+    echo 
     "<script>
-    alert('글쓰기 실패');
-    history.back();
-  </script>";
+        if (confirm('글을 등록하시겠습니까?')) {
+            location.href = '?aqid=" . $_POST['aqid'] . "&acontent=" . urlencode($_POST['acontent']) . "&confirm=true';
+        } else {
+            alert('등록이 취소되었습니다.');
+            location.href = 'admin_qna.php';
+        }
+    </script>";
 }
-
 ?>
