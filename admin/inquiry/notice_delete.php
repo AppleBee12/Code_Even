ffde2f683c66a11aa125df6de072a2bc42bc8657
YUNA9627 <yuna2175@naver.com>
2024-12-23 10:24:27 +0900

    📝 docs: confirm 취소 기능 추가 오류로 인한 삭제 기능 다시 수정

--- admin/inquiry/notice_delete.php
+++ admin/inquiry/notice_delete.php
@@ -1,35 +1,33 @@
 <?php
   include_once($_SERVER['DOCUMENT_ROOT'] . '/CODE_EVEN/admin/inc/dbcon.php');
 
-  // JavaScript를 사용해 확인창을 표시
-  if (!isset($_GET['delete']) || $_GET['delete'] !== 'true') {
-    echo "
-    <script>
-        if (confirm('글을 삭제하시겠습니까?')) {
-            location.href = 'notice.php?ntid=" . $_GET['ntid'] . "&delete=true';
-        } else {
-            alert('삭제가 취소되었습니다.');
-            location.href = 'notice.php';
-        }
-    </script>";
-    exit; // 아래 PHP 코드 실행 방지
-  }
-
+if (isset($_GET['ntid']) && isset($_GET['confirm']) && $_GET['confirm'] == "true") {
   $ntid = $_GET['ntid'];
   $sql = "DELETE FROM notice WHERE ntid = $ntid";
   $result = $mysqli->query($sql);
 
-  if($result){
+  if ($result) {
+  echo 
+  "<script>
+      alert('삭제가 완료되었습니다.');
+      location.href = 'notice.php?delete=true';
+  </script>";
+  } else {
     echo 
     "<script>
-      alert('삭제가 완료되었습니다.');
-      location.href='notice.php';
-    </script>";
-  }else{
-    echo
-    "<script>
-      alert('글 삭제 실패');
-      history.back();
+        alert('글 삭제 실패');
+        history.back();
     </script>";
   }
+} else {
+  echo 
+  "<script>
+      if (confirm('글을 삭제하시겠습니까?')) {
+          location.href = '?ntid=" . $_GET['ntid'] . "&confirm=true';
+      } else {
+          alert('삭제가 취소되었습니다.');
+          location.href = 'notice.php';
+      }
+  </script>";
+}
 ?>
