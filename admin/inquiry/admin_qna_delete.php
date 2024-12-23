4b6f749e92224fde51a112102ad4ee8a70c3d586
YUNA9627 <yuna2175@naver.com>
2024-12-23 10:27:25 +0900

    📝 docs: confirm 취소 기능 추가 오류로 인한 삭제 기능 다시 수정

--- admin/inquiry/admin_qna_delete.php
+++ admin/inquiry/admin_qna_delete.php
@@ -1,22 +1,33 @@
 <?php
 include_once($_SERVER['DOCUMENT_ROOT'] . '/CODE_EVEN/admin/inc/dbcon.php');
 
+if (isset($_GET['aqid']) && isset($_GET['confirm']) && $_GET['confirm'] == "true") {
 $aqid = $_GET['aqid'];
 $sql = "DELETE FROM admin_question WHERE aqid = $aqid";
 $result = $mysqli->query($sql);
 
-if ($result) {
-  echo
-    "<script>
-    confirm('글을 삭제하시겠습니까?');
-    alert('삭제가 완료되었습니다.');
-    location.href='admin_qna.php';
+  if ($result) {
+  echo 
+  "<script>
+      alert('삭제가 완료되었습니다.');
+      location.href = 'admin_qna.php?delete=true';
   </script>";
-} else {
-  echo
+  } else {
+    echo 
     "<script>
-      alert('글 삭제 실패');
-      history.back();
+        alert('글 삭제 실패');
+        history.back();
     </script>";
+  }
+} else {
+  echo 
+  "<script>
+      if (confirm('글을 삭제하시겠습니까?')) {
+          location.href = '?aqid=" . $_GET['aqid'] . "&confirm=true';
+      } else {
+          alert('삭제가 취소되었습니다.');
+          location.href = 'admin_qna.php';
+      }
+  </script>";
 }
 ?>
