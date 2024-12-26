694eb2632cd51437734183d8456822f7471d9ac6
AppleBee12 <mieldeluna@naver.com>
2024-12-27 05:10:44 +0900

    📝 docs: dbcon계정내용변경

--- admin/inc/dbcon.php
+++ admin/inc/dbcon.php
@@ -5,12 +5,11 @@ $password = '12345';
 $dbname = 'code_even';
 
 $mysqli = new mysqli($hostname, $username, $password, $dbname);
-if($mysqli->connect_errno){
+if ($mysqli->connect_errno) {
   throw new RuntimeException('연결에러' . $mysqli->connect_error);
 }
 
 $mysqli->set_charset('utf8mb4');
-if($mysqli->errno){
+if ($mysqli->errno) {
   throw new RuntimeException('연결 후 에러' . $mysqli->error);
 }
-
