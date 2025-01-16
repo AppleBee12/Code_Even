a0cc0ab224dc7d706019fd778bf7eb66001f14f1
eunjin6-6 <dmswls616@gmail.com>
2025-01-17 03:55:56 +0900

    📝 docs: 마이페이지-결제내역 페이지 생성

---
+++ front/mypage/mypage_payment_history.php
@@ -0,0 +1,27 @@
+<?php
+$title = '마이페이지-강좌보기';
+include_once($_SERVER['DOCUMENT_ROOT'] . '/CODE_EVEN/front/inc/mypage_header.php');
+
+?>
+<div>
+  <!--제목 시작-->
+  <div class="mypage_title_wrapper">
+    <p class="mypage_title headt5">결제내역</p>
+  </div>
+  <div>
+    <div class="pt-2">
+      <p>결제내역 페이지입니다.</p>
+    </div>
+  </div>
+
+  <!--제목 끝-->
+</div>
+
+
+</div><!--여기부터는 마이페이지 헤더의 닫는 태그-->
+</section>
+</div>
+</div>
+</div>
+<?php include_once($_SERVER['DOCUMENT_ROOT'] . '/CODE_EVEN/front/inc/footer.php');
+?>
