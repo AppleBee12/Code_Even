a797c114a397447d9bfdf020ea83e9e100ed60fe
AppleBee12 <mieldeluna@naver.com>
2025-02-13 23:35:38 +0900

    📝 docs: js파일이 html로드 후 실행하도록 $(document).ready(function(){});추가

--- front/js/what_recipe.js
+++ front/js/what_recipe.js
@@ -1,11 +1,14 @@
-// sec07 review
-
-let sec07Swiper = new Swiper('.sec07_swiper', {
-  loop: true,
-  slidesPerView: 5,
-  speed: 3000,
-  centeredSlides: true,
-  autoplay: {
-    delay: 1000
-  }
+$(document).ready(function () {
+  
+  // sec07 review
+  let sec07Swiper = new Swiper('.sec07_swiper', {
+    loop: true,
+    slidesPerView: 5,
+    speed: 3000,
+    centeredSlides: true,
+    autoplay: {
+      delay: 1000
+    }
+  });
+  
 });
