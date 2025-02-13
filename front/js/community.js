81ddbe45b1e1db630b4208d5509a53403445aff8
AppleBee12 <mieldeluna@naver.com>
2025-02-14 01:19:51 +0900

    🐛 Fix: '수정'클릭시 해당 댓글 수정폼이 열리도록 수정완료

--- front/js/community.js
+++ front/js/community.js
@@ -4,17 +4,21 @@ $(document).ready(function () {
   const commentModify = document.querySelectorAll(".comment-modify");
   commentModify.forEach((button) => {
     button.addEventListener("click", function (){
-      const commentItem = document.querySelector(".comment_write");
-      const commentText = commentItem.querySelector(".comment_dtext");
+      const commentItem = this.closest(".comment_write");
+      const commentText = commentItem.querySelector(".comment_text");
       const modifyBtn = commentItem.querySelector(".modify-btn");
       const commentEditForm = commentItem.querySelector(".comment-edit-form");
 
       //기존 댓글 숨기기 =  클릭시 comment_text, modify-btn display 숨기
       commentText.style.display = "none";
+      modifyBtn.classList.remove("d-flex");
       modifyBtn.style.display = "none";
 
-      //수정폼 보이기 = comment-modify는 display block
+      //수정폼 보이기 = comment-edit-form는 display block
       commentEditForm.style.display = "block";
+
+      //const commidInput = commentItem.querySelector("input[name='commid']");
+      //console.log("수정할 댓글 ID:", commidInput.value); // 디버깅용
     })
   });
 
