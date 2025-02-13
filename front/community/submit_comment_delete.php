b66ca02ef8a822ffdbba573dfacba4e47e069c08
AppleBee12 <mieldeluna@naver.com>
2025-02-14 03:37:30 +0900

    📝 docs: 총 댓글 갯수 -1,  post_comment테이블에서 해당 db삭제

--- front/community/submit_comment_delete.php
+++ front/community/submit_comment_delete.php
@@ -1,61 +1,50 @@
 <?php
 include_once($_SERVER['DOCUMENT_ROOT'] . '/code_even/admin/inc/dbcon.php');
 
+//print_r($_POST);
+//Array ( [commid] => 8 )
 
-print_r($_POST);
-//Array ( [uid] => 1 [board_type] => C [post_id] => 12 [contents] => 댓글 )
+$commid = $_POST['commid'];
+$uid = $_POST['uid'];
+$board_type = $_POST['board_type'];
+$post_id = $_POST['post_id'];
 
-// $uid = $_POST['uid'];
-// $board_type = $_POST['board_type'];
-// $post_id = $_POST['post_id'];
-// $contents = nl2br($_POST['contents']);
+$sql = "DELETE FROM post_comment 
+        WHERE commid = ? AND uid = ?";
 
+if ($stmt = $mysqli->prepare($sql)) {
+  $stmt->bind_param("ii", $commid, $uid);
 
-// $sql = "DELETE FROM post_comment (uid, board_type, post_id, contents) 
-//         VALUES ('$uid', '$board_type', '$post_id', '$contents')";
+// 글을 작성한 게시판, 글의 정보에 댓글 갯수 -1 만들기
+if ($stmt->execute()) {
+  $table_name = '';
+  if ($board_type === 'C') {
+    $table_name = 'counsel'; // 상담 게시판
+  } elseif ($board_type === 'B') {
+    $table_name = 'blog'; // 블로그 게시판
+  } elseif ($board_type === 'T') {
+    $table_name = 'teamproject'; // 팀 프로젝트 게시판
+  }
 
-// $result = $mysqli->query($sql);
-// //echo $sql;
+  if ($table_name) {
+    $sql_update_comments = "UPDATE $table_name SET comments = comments - 1 WHERE post_id = ?";
+    $stmt_update = $mysqli->prepare($sql_update_comments);
+    $stmt_update->bind_param("i", $post_id);
+    $stmt_update->execute();
+  }
 
-// // 글을 작성한 게시판, 글의 정보에 댓글 갯수 -1 만들기
-// if ($result) {
-//   $table_name = '';
-//   if ($board_type === 'C') {
-//     $table_name = 'counsel'; // 상담 게시판
-//   } elseif ($board_type === 'B') {
-//     $table_name = 'blog'; // 블로그 게시판
-//   } elseif ($board_type === 'T') {
-//     $table_name = 'teamproject'; // 팀 프로젝트 게시판
-//   }
-
-//   if ($table_name) {
-//     $sql_update_comments = "UPDATE $table_name SET comments = comments - 1 WHERE post_id = ?";
-//     $stmt_update = $mysqli->prepare($sql_update_comments);
-//     $stmt_update->bind_param("i", $post_id);
-//     $stmt_update->execute();
-//   }
-
-//   echo
-//   "<script>
-//         if (confirm('댓글을 삭제하시겠습니까?')){
-        
-//           alert('삭제가 완료되었습니다.');
-//           location.href='" . ($board_type === 'C' ? 'counsel.php' : ($board_type === 'B' ? 'blog.php' : 'teamproject.php')) . "';
-        
-//       }else{
-//         alert('취소되었습니다.');
-//         history.back();
-//       } 
-//     </script>";
-// } else {
-//   echo
-//   "<script>
-//       alert('댓글 삭제가 실패했습니다! 다시 시도해주세요!');
-//       history.back();
-//     </script>";
-// }
-
-//$result->close();
+            // 응답 성공 메시지
+            echo json_encode(['success' => true, 'message' => '댓글이 삭제되었습니다.']);
+        } else {
+            // 오류 메시지
+            echo json_encode(['success' => false, 'message' => '댓글 삭제에 실패했습니다.']);
+        }
+        $stmt->close();
+    } else {
+        echo json_encode(['success' => false, 'message' => '쿼리 준비 실패']);
+    }
 
 
+
+$mysqli->close();
 ?>
