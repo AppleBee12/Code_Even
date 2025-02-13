1d55b537bb6d95e8bbb8aea841af7013a9f5f145
AppleBee12 <mieldeluna@naver.com>
2025-02-14 00:25:46 +0900

    🐛 Fix: '수정'클릭시 첫번째 댓글의 수정폼이 열리는 오류발생, 수정중

--- front/js/community.js
+++ front/js/community.js
@@ -1,30 +1,22 @@
 $(document).ready(function () {
 
   //댓글 수정 클릭시 작동
-    document.querySelectorAll(".comment-modify").forEach((btn) => {
-    btn.addEventListener("click", function (){
-      const commentItem = this.closest(".comment_write");
-      const commentText = commentItem.querySelector(".comment-text");
+  const commentModify = document.querySelectorAll(".comment-modify");
+  commentModify.forEach((button) => {
+    button.addEventListener("click", function (){
+      const commentItem = document.querySelector(".comment_write");
+      const commentText = commentItem.querySelector(".comment_dtext");
       const modifyBtn = commentItem.querySelector(".modify-btn");
       const commentEditForm = commentItem.querySelector(".comment-edit-form");
 
-      
       //기존 댓글 숨기기 =  클릭시 comment_text, modify-btn display 숨기
-      //수정폼 보이기 = comment-modify는 display block
-
       commentText.style.display = "none";
       modifyBtn.style.display = "none";
+
+      //수정폼 보이기 = comment-modify는 display block
       commentEditForm.style.display = "block";
     })
   });
-
-    //댓글 수정 취소 클릭시 작동
-    //반대로
-   
-
-
-
-
 
 
 
