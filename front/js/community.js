0a6b5ae47c9569578ea34e86de3b0c78139c67f8
AppleBee12 <mieldeluna@naver.com>
2025-02-13 23:53:08 +0900

    📝 docs: 댓글 수정기능 제작중

--- front/js/community.js
+++ front/js/community.js
@@ -1,8 +1,14 @@
 $(document).ready(function () {
   document.querySelectorAll(".comment-modify").forEach((btn) => {
     btn.addEventListener("click", function (){
+      const commentItem = this.closest(".comment_write");
+      const commentText = commentItem.querySelector(".comment-text");
+      const modifyBtn = commentItem.querySelector(".modify-btn");
+      const commentModify = commentItem.querySelector(".comment-modify");
+
+      
       //기존 댓글 숨기기 =  클릭시 comment_text, modify-btn display 숨기
-      //수정폼 보이기 = commtent-modify는 display block
+      //수정폼 보이기 = comment-modify는 display block
     })
   })
 
