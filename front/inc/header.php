51bc99d96ccc7071d33bc784b01bc0dc44dd6293
AppleBee12 <mieldeluna@naver.com>
2025-03-03 21:41:07 +0900

    📝docs: tab이 클릭된 상태로 이동할 수 있도록 datatab="lecture","service설정"진행 중

--- front/inc/header.php
+++ front/inc/header.php
@@ -656,8 +656,11 @@ if (isset($_GET['code'])) {
                 <div class="mncart_list alert">
                   <div class="alert new_answer text-center" role="alert">
                     <?php if ($new_answers > 0): ?>
-                      <p><i class="bi bi-info-circle-fill"> </i>확인하지 않은 새 답변이 <a href="http://<?= $_SERVER['HTTP_HOST']; ?>/code_even/front/mypage/mypage_qna.php"
+                      <p><i class="bi bi-info-circle-fill"> </i>확인하지 않은 새 강의 문의 답변이 <a href="http://<?= $_SERVER['HTTP_HOST']; ?>/code_even/front/mypage/mypage_qna.php?tab=lecture"
                       class="alert-link"><span><?= $new_answers ?>건</span></a> 있습니다.</p>
+                    <?php elseif ($new_service_answers > 0): ?>
+                      <p><i class="bi bi-info-circle-fill"> </i>확인하지 않은 새 서비스 문의 답변이 <a href="http://<?= $_SERVER['HTTP_HOST']; ?>/code_even/front/mypage/mypage_qna.php?tab=service"
+                      class="alert-link"><span><?= $new_service_answers ?>건</span></a> 있습니다.</p>
                     <?php else: ?>
                       <p>새로운 알림이 없습니다.</p>
                     <?php endif; ?>
